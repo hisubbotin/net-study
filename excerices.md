0ea351517c7bec1f31c990056e9f0a3bf3aaa18e
pkuderov <pkuderov@gmail.com>
2017-09-11 14:32:07 +0300

    initial on exercises

---
+++ excerices.md
@@ -0,0 +1,10 @@
+# Excercises
+
+## Типы данных, значимые типы
+
+1. Integers
+    * constants MinValue, MaxValue
+    * type overflow, explicit checked/unchecked mode
+1. Float numbers
+    * constants MinValue, MaxValue, PositiveInfinity, NegativeInfinity, Epsilon, NaN
+    * when result is NaN, how to check NaN/Infinity.
---
+++ exercises/Numbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Numbers.Tests")]
+
+namespace Numbers
+{
+    internal static class Program
+    {
+
+        private static void Main()
+        {
+            TestDecimal();
+            TestDouble();
+
+            Console.WriteLine(-double.Epsilon);
+            Console.WriteLine(0d / 0d);
+        }
+
+        private static void TestDecimal()
+        {
+            var d1 = decimal.One;
+            var d2 = 18.3m;
+
+            var res = d1 / d2;
+            Console.WriteLine(res);
+            res *= 123137123m;
+            Console.WriteLine(res);
+            res = decimal.Remainder(18.7m, 13m);
+            Console.WriteLine(res);
+        }
+
+        private static void TestDouble()
+        {
+            var d1 = 1.0;
+            var d2 = 18.3;
+            var res = d1 / d2;
+            Console.WriteLine(res);
+            res *= 123137123;
+            Console.WriteLine(res);
+            var rem = 18.7 - 13 * Math.Floor(18.7 / 13);
+            Console.WriteLine(rem);
+        }
+    }
+}
