8fe74c887897d8504eed56b2348a7683f75aa331
Petr Kuderov <Petr_Ku@abbyy.com>
2017-09-24 16:14:13 +0300

    [01-primitive-types]: добавил задач в Integers и FloatNumbers, комментарии и тесты.

--- exercises/01-primitive-types/Numbers/Numbers.Tests/IntegersTest.cs
+++ exercises/01-primitive-types/Numbers/Numbers.Tests/IntegersTest.cs
@@ -8,8 +8,7 @@ namespace Numbers.Tests
         [Fact]
         public void Test_HalfIntMaxValue()
         {
-            var x = Integers.HalfIntMaxValue();
-            Assert.Equal(x, int.MaxValue / 2);
+            Assert.Equal(Integers.HalfIntMaxValue(), 1073741823);
         }
 
         [Theory]
@@ -31,8 +30,8 @@ namespace Numbers.Tests
         }
 
         [Theory]
-        [InlineData(int.MaxValue)]
-        [InlineData(int.MinValue/2)]
+        [InlineData(2147483646)]
+        [InlineData(-1073741824)]
         public void Test_CubeWithOverflowCheck_ThrowsArithmeticOverflowException(int x)
         {
             Assert.Throws<OverflowException>(() => Integers.CubeWithOverflowCheck(x));
@@ -46,12 +45,52 @@ namespace Numbers.Tests
         {
             Assert.Equal(Integers.CubeWithoutOverflowCheck(x), x * x * x);
         }
+
         [Theory]
-        [InlineData(int.MaxValue)]
-        [InlineData(int.MinValue / 2)]
+        [InlineData(2147483646)]
+        [InlineData(-1073741824)]
         public void Test_CubeWithoutOverflowCheck_ShouldNotThrow(int x)
         {
             Assert.Equal(Integers.CubeWithoutOverflowCheck(x), x * x * x);
+        }
+
+        [Theory]
+        [InlineData(0, "0")]
+        [InlineData(-123456, "-123456")]
+        [InlineData(123456789, "123456789")]
+        [InlineData(10.123, "10")]
+        public void Test_ToString_ShouldReturnCorrectValue(int x, string xStr)
+        {
+            Assert.Equal(Integers.ToString(x), xStr);
+        }
+
+        [Theory]
+        [InlineData("0", 0)]
+        [InlineData("-123456", -123456)]
+        [InlineData("123456789", 123456789)]
+        public void Test_Parse_ShouldReturnCorrectValue(string xStr, int x)
+        {
+            Assert.Equal(Integers.Parse(xStr), x);
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 10)]
+        [InlineData(-5, -50)]
+        public void Test_TenTimes_ShouldReturnCorrectValue(int x, int tenTimesX)
+        {
+            Assert.Equal(Integers.TenTimes(x), tenTimesX);
+        }
+
+        [Theory]
+        [InlineData(0, "0")]
+        [InlineData(1, "1")]
+        [InlineData(14, "E")]
+        [InlineData(12315, "301B")]
+        [InlineData(-289823, "FFFB93E1")]
+        public void Test_ToHexString_ShouldReturnCorrectValue(int x, string xHexStr)
+        {
+            Assert.Equal(Integers.ToHexString(x), xHexStr, ignoreCase: true);
         }
     }
 }
