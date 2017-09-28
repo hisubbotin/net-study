584a29ed5e5c39a0887340f7b5ab2a12ca5c0321
Zufar <lazuka13@gmail.com>
2017-09-28 17:04:46 +0300

    Solved 01

--- exercises/01-primitive-types/Numbers/Numbers/FloatNumbers.cs
+++ exercises/01-primitive-types/Numbers/Numbers/FloatNumbers.cs
@@ -8,6 +8,9 @@ namespace Numbers
 {
     public static class FloatNumbers
     {
+
+        static double zero; // для метода GetNaN
+
         /// <summary>
         /// Возвращает результат проверки, имеет ли указанное вещественнозначное число значение "не число" (Not a number).
         /// </summary>
@@ -15,8 +18,7 @@ namespace Numbers
         /// <returns>True, если число имеет значение "не число", иначе false.</returns>
         internal static bool IsNaN(double d)
         {
-            // Подсказка: по аналогии с константами типа int, у типа double тоже есть свой набор констант.
-            throw new NotImplementedException();
+            return double.IsNaN(d);
         }
 
         /// <summary>
@@ -28,25 +30,28 @@ namespace Numbers
                 Необходимо вернуть значение, не используя непосредственно саму константу.
                 Для этого подумай, какой смысл в себе несет эта константа и где бы она могла стать результатом операции или вычисления функции.
             */
-            throw new NotImplementedException();
+            return 0 / zero;
         }
 
         /// <summary>
         /// Возвращает результат сравнения двух вещественнозначных чисел.
         /// </summary>
         /// <returns>-1 - первое меньше второго, 0 - значения равны, 1 - первое больше второго.</returns>
-        internal static int Compare(/* дополни сигнатуру метода как считаешь правильным */)
+        internal static int Compare(double first, double second, double precision = 0.0001)
         {
-            /*
-                Подумай, почему это задание дано в части про вещественнозначные числа. И почему не дана полная сигнатура метода.
-                Если сходу идей нет, перестань искать подвох и просто реализуй дословно. Теперь еще раз посмотри на код и подумай в чем может быть проблема, сколько должно быть аргументов.
-            */
-            throw new NotImplementedException();
+            if (Math.Abs(first - second) < precision)
+            {
+                return 0;
+            }
+            else if (first > second)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
         }
 
-        // и все?!! О_о
-
-
-        // и все... -_-
     }
 }
