71e37272e61f9dae48c6a2d8caa7a6faa8184209
NikitaSerenko <nikita.serenko@phystech.edu>
2017-10-08 01:20:02 +0300

    hw 1 + 2(part) (Nikita Serenko 493)

--- exercises/02-adventure-time/AdventureTime/AdventureTime/Time.cs
+++ exercises/02-adventure-time/AdventureTime/AdventureTime/Time.cs
@@ -6,53 +6,52 @@ namespace AdventureTime
     {
         internal static DateTime WhatTimeIsIt()
         {
-            throw new NotImplementedException();
+            return DateTime.Now;
         }
 
         internal static DateTime WhatTimeIsItInUtc()
         {
-            throw new NotImplementedException();
+            return DateTime.UtcNow;
         }
 
         internal static DateTime ParseDateTime(string dtStr)
         {
-            throw new NotImplementedException();
+            return DateTime.Parse(dtStr, System.Globalization.CultureInfo.InvariantCulture);
         }
 
         internal static DateTime ToUtc(DateTime dt)
         {
-            throw new NotImplementedException();
+            return dt.ToUniversalTime();
         }
 
         internal static DateTime AddTenSeconds(DateTime dt)
         {
-            throw new NotImplementedException();
+            return dt.Add(new TimeSpan(0, 0, 10));   
         }
 
         internal static int GetHoursBetween(DateTime dt1, DateTime dt2)
         {
-            throw new NotImplementedException();
+            TimeSpan diff = dt1 - dt2;
+            return diff.Hours;
         }
 
         internal static int GetTotalMinutesInThreeMonths()
         {
-            throw new NotImplementedException();
+             return new TimeSpan (30*3 /*probably*/, 0, 0,  0).Minutes; 
         }
 
         internal static int GetParticularTravelDurationInMinutes()
         {
-            /*
                 
-                var fromLondon = new DateTimeOffset(2010, 3, 28, 1, 15, 0, TimeSpan.FromHours(1));
-                var toMoscow = new DateTimeOffset(2010, 3, 28, 5, 0, 0, TimeSpan.FromHours(4));
-
-            */
-            throw new NotImplementedException();
+            var fromLondon = new DateTimeOffset(2010, 3, 28, 1, 15, 0, TimeSpan.FromHours(1));
+            var toMoscow = new DateTimeOffset(2010, 3, 28, 5, 0, 0, TimeSpan.FromHours(3));
+            return toMoscow.Subtract(fromLondon).Minutes;
         }
 
         internal static void GetBirthdate()
         {
-            throw new NotImplementedException();
+            //C# First appeared: 2000; 17 years ago
+            return;
         }
     }
 }
