62df1ad26f466930d43355c5e107cacb6f37a673
Sdernal <sdernal2@gmail.com>
2017-10-08 17:29:59 +0300

    1st and 2nd tasks

--- exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -9,22 +9,22 @@ namespace AdventureTime
         {
             Console.WriteLine("Hello World!");
 
-            //Console.WriteLine(DateTime.MinValue);
-            //Console.WriteLine(DateTime.MaxValue);
+            Console.WriteLine(DateTime.MinValue);
+            Console.WriteLine(DateTime.MaxValue);
 
-            //Console.WriteLine(DateTime.Now);
-            //Console.WriteLine(DateTime.UtcNow.Kind);
-            //Console.WriteLine(new DateTime(2017, 10, 18).Kind);
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.UtcNow.Kind);
+            Console.WriteLine(new DateTime(2017, 10, 18).Kind);
 
-            //Console.WriteLine(DateTime.Now.ToUniversalTime());
-            //Console.WriteLine(DateTime.UtcNow.ToUniversalTime());
-            //Console.WriteLine(new DateTime(2017, 10, 18).ToUniversalTime());
+            Console.WriteLine(DateTime.Now.ToUniversalTime());
+            Console.WriteLine(DateTime.UtcNow.ToUniversalTime());
+            Console.WriteLine(new DateTime(2017, 10, 18).ToUniversalTime());
 
-            //Console.WriteLine(DateTimeOffset.MinValue);
-            //Console.WriteLine(DateTimeOffset.MaxValue);
+            Console.WriteLine(DateTimeOffset.MinValue);
+            Console.WriteLine(DateTimeOffset.MaxValue);
 
-            //Console.WriteLine(TimeSpan.MinValue);
-            //Console.WriteLine(new TimeSpan(10, 20, 13, 100).Minutes);
+            Console.WriteLine(TimeSpan.MinValue);
+            Console.WriteLine(new TimeSpan(10, 20, 13, 100).Minutes);
 
             var gmt = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
             var msd = TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time");
@@ -39,6 +39,20 @@ namespace AdventureTime
 
             Console.WriteLine(toMoscow - fromLondon);
             Console.WriteLine(tM - fL);
+
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine(Time.WhatTimeIsIt());
+            Console.WriteLine(Time.WhatTimeIsItInUtc());
+            Console.WriteLine(Time.ParseDateTime("12/12/2012"));
+            Console.WriteLine(Time.ToUtc(DateTime.Now));
+            Console.WriteLine(Time.AddTenSeconds(Time.ParseDateTime("12/12/2012")));
+            Console.WriteLine(Time.GetHoursBetween(DateTime.Now, Time.ToUtc(DateTime.Now)));
+            Console.WriteLine((DateTime.Now - DateTime.UtcNow).TotalHours);
+            Console.WriteLine(Time.GetTotalMinutesInThreeMonths());
+            Console.WriteLine(Time.GetParticularTravelDurationInMinutes());
+            Console.WriteLine(Time.GetBirthdate());
+
         }
     }
 }
--- exercises/02-adventure-time/AdventureTime/AdventureTime/Time.cs
+++ exercises/02-adventure-time/AdventureTime/AdventureTime/Time.cs
@@ -6,53 +6,64 @@ namespace AdventureTime
     {
         internal static DateTime WhatTimeIsIt()
         {
-            throw new NotImplementedException();
+            return DateTime.Now;
+            //throw new NotImplementedException();
         }
 
         internal static DateTime WhatTimeIsItInUtc()
         {
-            throw new NotImplementedException();
+            return DateTime.UtcNow;
+            //throw new NotImplementedException();
         }
 
         internal static DateTime ParseDateTime(string dtStr)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return DateTime.Parse(dtStr);
         }
 
         internal static DateTime ToUtc(DateTime dt)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return dt.ToUniversalTime();
         }
 
         internal static DateTime AddTenSeconds(DateTime dt)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return dt.AddSeconds(10);
         }
 
         internal static int GetHoursBetween(DateTime dt1, DateTime dt2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            TimeSpan delta = dt1 - dt2;
+            return delta.Hours;
         }
 
         internal static int GetTotalMinutesInThreeMonths()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            DateTime dt1 = new DateTime();
+            DateTime dt2 = dt1.AddMonths(3);
+            TimeSpan ts = dt2 - dt1;
+            return (int)ts.TotalMinutes;
         }
 
         internal static int GetParticularTravelDurationInMinutes()
         {
-            /*
-                
-                var fromLondon = new DateTimeOffset(2010, 3, 28, 1, 15, 0, TimeSpan.FromHours(1));
-                var toMoscow = new DateTimeOffset(2010, 3, 28, 5, 0, 0, TimeSpan.FromHours(4));
+            var fromLondon = new DateTimeOffset(2010, 3, 28, 1, 15, 0, TimeSpan.FromHours(1));
+            var toMoscow = new DateTimeOffset(2010, 3, 28, 5, 0, 0, TimeSpan.FromHours(4));
 
-            */
-            throw new NotImplementedException();
+            TimeSpan duration = toMoscow - fromLondon;
+            return (int)duration.TotalMinutes;
+            //throw new NotImplementedException();
         }
 
-        internal static void GetBirthdate()
+        internal static DateTime GetBirthdate()
         {
-            throw new NotImplementedException();
-        }
+            //throw new NotImplementedException();
+            return new DateTime(year: 1994, month: 10, day: 12);
+        }   
     }
 }
