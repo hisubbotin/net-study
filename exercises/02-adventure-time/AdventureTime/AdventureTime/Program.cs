e6e6c93e55ef74296ba4cee1741479bfd5e21b84
NikitaSerenko <nikita.serenko@phystech.edu>
2017-10-10 19:57:24 +0300

    hw 2(Nikita Serenko 493)

--- exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -1,44 +1,16 @@
 ﻿using System;
-using System.Linq;
-
 namespace AdventureTime
 {
     internal class Program
     {
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
-
-            //Console.WriteLine(DateTime.MinValue);
-            //Console.WriteLine(DateTime.MaxValue);
-
-            //Console.WriteLine(DateTime.Now);
-            //Console.WriteLine(DateTime.UtcNow.Kind);
-            //Console.WriteLine(new DateTime(2017, 10, 18).Kind);
-
-            //Console.WriteLine(DateTime.Now.ToUniversalTime());
-            //Console.WriteLine(DateTime.UtcNow.ToUniversalTime());
-            //Console.WriteLine(new DateTime(2017, 10, 18).ToUniversalTime());
-
-            //Console.WriteLine(DateTimeOffset.MinValue);
-            //Console.WriteLine(DateTimeOffset.MaxValue);
-
-            //Console.WriteLine(TimeSpan.MinValue);
-            //Console.WriteLine(new TimeSpan(10, 20, 13, 100).Minutes);
-
-            var gmt = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
-            var msd = TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time");
-
-            var fromLondon = new DateTimeOffset(2010, 3, 28, 1, 15, 0, TimeSpan.FromHours(1));
-            var toMoscow = new DateTimeOffset(2010, 3, 28, 5, 0, 0, TimeSpan.FromHours(4));
-
-            var fL = TimeZoneInfo.ConvertTime(fromLondon, gmt);
-            var tM = TimeZoneInfo.ConvertTime(toMoscow, msd);
-
-            Console.WriteLine(fL.LocalDateTime);
-
-            Console.WriteLine(toMoscow - fromLondon);
-            Console.WriteLine(tM - fL);
+            DateTime d = DateTime.Now;
+            DateTime s = d.AddMonths(1);
+            System.Console.WriteLine(Time.AreEqualBirthdays(d, d));
+            System.Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+            System.Console.WriteLine(Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+            System.Console.ReadLine();
         }
     }
 }
