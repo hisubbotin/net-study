0b8bf79fb01e3799be42487ad2685b4722fddfd9
Ivan Tregubov <iat7@ya.ru>
2017-10-10 19:40:52 +0300

    1st task

--- exercises/01. Primitive types/Numbers/Numbers/FloatNumbers.cs
+++ exercises/01. Primitive types/Numbers/Numbers/FloatNumbers.cs
@@ -1,7 +1,67 @@
-﻿namespace Numbers
+﻿/*
+    Здесь все то же самое.
+*/
+
+using System;
+
+namespace Numbers
 {
     public static class FloatNumbers
     {
+        /// <summary>
+        /// Возвращает результат проверки, имеет ли указанное вещественнозначное число значение "не число" (Not a number).
+        /// </summary>
+        /// <param name="d">Проверяемое вещественнозначное число.</param>
+        /// <returns>True, если число имеет значение "не число", иначе false.</returns>
+        internal static bool IsNaN(double d)
+        {
+            // Подсказка: по аналогии с константами типа int, у типа double тоже есть свой набор констант.D:\DotNetCourse\net-study\exercises\01-primitive-types\Numbers\Numbers\FloatNumbers.cs
+            return double.IsNaN(d);
 
+        }
+
+        /// <summary>
+        /// Возвращает вещественнозначное число со значением "не число" (Not a number).
+        /// </summary>
+        internal static double GetNaN()
+        {
+            /*
+                Необходимо вернуть значение, не используя непосредственно саму константу.
+                Для этого подумай, какой смысл в себе несет эта константа и где бы она могла стать результатом операции или вычисления функции.
+            */
+            double zero = 0.0;
+            return zero / zero;
+            //throw new NotImplementedException(
+        }
+
+        /// <summary>
+        /// Возвращает результат сравнения двух вещественнозначных чисел.
+        /// </summary>
+        /// <returns>-1 - первое меньше второго, 0 - значения равны, 1 - первое больше второго.</returns>
+        internal static int Compare(double first, double second, double epsilon)
+        {
+            /*
+                Подумай, почему это задание дано в части про вещественнозначные числа. И почему не дана полная сигнатура метода.
+                Если сходу идей нет, перестань искать подвох и просто реализуй дословно. Теперь еще раз посмотри на код и подумай в чем может быть проблема, сколько должно быть аргументов.
+            */
+
+                        if (first < second - epsilon)
+                        {
+                                return 1;
+                        }
+                        else if (first > second + epsilon)
+                        {
+                                return -1;
+                        }
+                        else
+                        {
+                                return 0;
+                        }
+        }
+
+        // и все?!! О_о
+
+
+        // и все... -_-
     }
 }
