167ccde6a858509ace6cefcbe1ea7363a7ae47c6
Petr Kuderov <pkuderov@gmail.com>
2017-10-10 18:11:00 +0300

    [02-adventure-time]: фикс идентификаторов зон
    Было не совсем то, что нужно)

--- exercises/02-adventure-time/AdventureTime/AdventureTime/Time.cs
+++ exercises/02-adventure-time/AdventureTime/AdventureTime/Time.cs
@@ -202,8 +202,8 @@ namespace AdventureTime
                 ниже ты найдешь готовый метод GetZonedTime. Просто посмотри на него (можешь даже посмотреть методы и свойства типа TimeZoneInfo, если интересно) и воспользуйся им для вычисления правильного времени
                 "отбытия" и "прибытия" наших героев. Затем посчитай длительность путешествия. Также даны правильные идентификаторы зон.
             */
-            var moscowZoneId = TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time");
-            var londonZoneId = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+            const string moscowZoneId = "Russian Standard Time";
+            const string londonZoneId = "GMT Standard Time";
 
             throw new NotImplementedException();
         }
@@ -216,8 +216,8 @@ namespace AdventureTime
             /*
                 Реши по аналогии с предыдущим методом и проверь, что оба метода действительно возвращают одно и то же время (и что оно правильное).
             */
-            var moscowZoneId = TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time");
-            var londonZoneId = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+            const string moscowZoneId = "Russian Standard Time";
+            const string londonZoneId = "GMT Standard Time";
             throw new NotImplementedException();
         }
 
