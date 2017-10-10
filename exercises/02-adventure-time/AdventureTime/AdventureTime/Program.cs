cae507964a7b1b9ab6503491fddbd56f342775a7
NikitaSerenko <nikita.serenko@phystech.edu>
2017-10-10 19:48:59 +0300

    hw 2(Nikita Serenko 493)

--- exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace AdventureTime
+﻿using System;
+namespace AdventureTime
 {
     internal class Program
     {
         private static void Main()
         {
+            DateTime d = DateTime.Now;
+            DateTime s = d.AddMonths(1);
+            System.Console.WriteLine(Time.AreEqualBirthdays(d, d));
+            System.Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+            System.Console.WriteLine(Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+            System.Console.ReadLine();
         }
     }
 }
