46eff7f9e50e335559441aa3ec06f81c45a51e19
Sdernal <sdernal2@gmail.com>
2017-10-16 00:09:04 +0300

    2nd task

--- exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -1,55 +1,32 @@
-﻿namespace AdventureTime
+﻿using System;
+
+namespace AdventureTime
 {
     internal class Program
     {
         private static void Main()
-        {
-            Console.WriteLine("Hello World!");
+        {         
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine(currentTime);
+            string roundTrip = Time.ToRoundTripFormatString(currentTime);
+            Console.WriteLine(roundTrip);
+            Console.WriteLine(Time.ParseFromRoundTripFormat(roundTrip));
 
-            Console.WriteLine(DateTime.MinValue);
-            Console.WriteLine(DateTime.MaxValue);
-
-            Console.WriteLine(DateTime.Now);
-            Console.WriteLine(DateTime.UtcNow.Kind);
-            Console.WriteLine(new DateTime(2017, 10, 18).Kind);
-
-            Console.WriteLine(DateTime.Now.ToUniversalTime());
-            Console.WriteLine(DateTime.UtcNow.ToUniversalTime());
-            Console.WriteLine(new DateTime(2017, 10, 18).ToUniversalTime());
-
-            Console.WriteLine(DateTimeOffset.MinValue);
-            Console.WriteLine(DateTimeOffset.MaxValue);
-
-            Console.WriteLine(TimeSpan.MinValue);
-            Console.WriteLine(new TimeSpan(10, 20, 13, 100).Minutes);
-
-            var gmt = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
-            var msd = TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time");
-
-            var fromLondon = new DateTimeOffset(2010, 3, 28, 1, 15, 0, TimeSpan.FromHours(1));
-            var toMoscow = new DateTimeOffset(2010, 3, 28, 5, 0, 0, TimeSpan.FromHours(4));
-
-            var fL = TimeZoneInfo.ConvertTime(fromLondon, gmt);
-            var tM = TimeZoneInfo.ConvertTime(toMoscow, msd);
-
-            Console.WriteLine(fL.LocalDateTime);
-
-            Console.WriteLine(toMoscow - fromLondon);
-            Console.WriteLine(tM - fL);
-
-            Console.WriteLine("------------------------------");
-
-            Console.WriteLine(Time.WhatTimeIsIt());
-            Console.WriteLine(Time.WhatTimeIsItInUtc());
-            Console.WriteLine(Time.ParseDateTime("12/12/2012"));
-            Console.WriteLine(Time.ToUtc(DateTime.Now));
-            Console.WriteLine(Time.AddTenSeconds(Time.ParseDateTime("12/12/2012")));
-            Console.WriteLine(Time.GetHoursBetween(DateTime.Now, Time.ToUtc(DateTime.Now)));
-            Console.WriteLine((DateTime.Now - DateTime.UtcNow).TotalHours);
+            Console.WriteLine(Time.GetHoursBetween(DateTime.Now, DateTime.Now +
+                TimeSpan.FromHours(2) + TimeSpan.FromDays(10))); // = -2
+            Console.WriteLine(Time.GetHoursBetween(DateTime.Now, DateTime.UtcNow)); // = 2
             Console.WriteLine(Time.GetTotalMinutesInThreeMonths());
-            Console.WriteLine(Time.GetParticularTravelDurationInMinutes());
-            Console.WriteLine(Time.GetBirthdate());
-
+            Console.WriteLine(Time.AreEqualBirthdays(new DateTime(1891, 5, 1, 4, 3, 0,DateTimeKind.Unspecified),
+                new DateTime(1712, 5, 1, 11, 30, 0, DateTimeKind.Unspecified)));
+            var someDate = new DateTime(2010, 1, 1, 0, 0, 0, DateTimeKind.Unspecified);
+            Console.WriteLine(someDate.ToUniversalTime()); // 31.12.2009 21:00:00 KEK
+            Console.WriteLine("ADVENTURE TIME!!!");
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver0_Dumb()); // 180
+            Console.WriteLine(Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver0_Dumb()); // 60
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver1_FeelsSmarter()); // 180 
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience()); // 120
+            Console.WriteLine(Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience()); // 120
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver3_NodaTime()); // 120
         }
     }
 }
