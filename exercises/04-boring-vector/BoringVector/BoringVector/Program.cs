79c7759ebb7184c88d884f202821fafca239feed
pkuderov <pkuderov@gmail.com>
2017-10-19 05:57:53 +0300

    [04-boring-vector]: добавил задание с описанием.

---
+++ exercises/04-boring-vector/BoringVector/BoringVector/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BoringVector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Foo.DoNothing(1);
+        }
+    }
+}
