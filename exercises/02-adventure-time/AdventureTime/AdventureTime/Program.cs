04a2924d1ac0ec1f3f3d794b42549cba59415627
nik.pashmentov@ya.ru <nik.pashmentov@ya.ru>
2017-10-24 22:33:37 +0300

    Решение второго задания

--- exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace AdventureTime
+﻿using System;
+
+namespace AdventureTime
 {
     internal class Program
     {
         private static void Main()
         {
+            var now = Time.WhatTimeIsIt();
+
+            var nowUTC = Time.WhatTimeIsItInUtc();
+
+            /*DateTime now = Time.WhatTimeIsIt();
+            String nowStr = Time.ToRoundTripFormatString(now);
+            DateTime now2 = Time.ParseFromRoundTripFormat(nowStr);
+            Console.WriteLine(Time.ToRoundTripFormatString(now));
+            Console.WriteLine(Time.ToRoundTripFormatString(now2));*/
+
+            /*var nowUTC = Time.ToUtc(now);
+            Console.WriteLine(Time.ToRoundTripFormatString(nowUTC));*/
+
+            //Console.WriteLine(Time.GetHoursBetween(now, nowUTC)); -> -2?
+
+            //Console.WriteLine(Time.GetTotalMinutesInThreeMonths());
+
+            //Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver0_Dumb());
+
+            /*Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver0_Dumb());
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver1_FeelsSmarter());*/
+
+            //Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience()); -> 120
         }
     }
 }
