8ac0d71e0284c3a35e3fc7b0d7219015a4ba729b
Semyon Kozlov <semkozloff@gmail.com>
2017-10-30 20:29:58 +0300

    [04-boring-vector]: add comments

--- exercises/04-boring-vector/BoringVector/BoringVector/VectorExtensions.cs
+++ exercises/04-boring-vector/BoringVector/BoringVector/VectorExtensions.cs
@@ -2,6 +2,9 @@
 
 namespace BoringVector
 {
+    /// <summary>
+    /// Взаимное расположение векторов в просранстве.
+    /// </summary>
     internal enum VectorRelation
     {
         General = 0,
@@ -9,19 +12,42 @@ namespace BoringVector
         Orthogonal = 2
     }
 
+    /// <summary>
+    /// Расширение функционалности класса <see cref="Vector"/>.
+    /// </summary>
     internal static class VectorExtenstions
     {
         private const double EPS = 1e-6;
 
+        /// <summary>
+        /// Проверяет, является ли вектор нулевым с точностью 1e-6.
+        /// </summary>
+        /// <param name="v">Объект типа <see cref="Vector"/>.</param>
+        /// <returns>True, если вектор имеет нулевые координаты, false иначе.</returns>
         public static bool IsZero(this Vector v) => Math.Abs(v.X) < EPS && Math.Abs(v.Y) < EPS;
 
+        /// <summary>
+        /// Возвращает норму вектора.
+        /// </summary>
+        /// <param name="v">Объект типа <see cref="Vector"/>.</param>
+        /// <returns>Норма вектора.</returns>
         public static double Length(this Vector v) => Math.Sqrt(v.SquareLength());
 
+        /// <summary>
+        /// Нормализует вектор <paramref name="v"/>.
+        /// </summary>
+        /// <param name="v">Объект типа <see cref="Vector"/>.</param>
         public static void Normalize(this Vector v)
         {
             v = v / Math.Sqrt(v.SquareLength());
         }
 
+        /// <summary>
+        /// Возвращает угол между двумя векторами в радианах.
+        /// </summary>
+        /// <param name="v">Объект типа <see cref="Vector"/>.</param>
+        /// <param name="v">Ещё объект типа <see cref="Vector"/>.</param>
+        /// <returns></returns>
         public static double GetAngleBetween(this Vector v, Vector u)
         {
             if (v.IsZero() || u.IsZero())
@@ -32,6 +58,12 @@ namespace BoringVector
             return Math.Acos(v.DotProduct(u) / (v.Length() * u.Length()));
         }
 
+        /// <summary>
+        /// Возвращает значение типа <see cref="VectorRelation"/> - отношение между векторами <paramref name="v"/> и <paramref name="u"/>.
+        /// </summary>
+        /// <param name="v">Объект типа <see cref="Vector"/>.</param>
+        /// <param name="v">Ещё объект типа <see cref="Vector"/>.</param>
+        /// <returns>Значение типа <see cref="VectorRelation"/> - отношение между двумя векторами.</returns>
         public static VectorRelation GetRelation(this Vector v, Vector u)
         {
             if (v.DotProduct(u) < EPS)
