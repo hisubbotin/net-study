8c3f43f042bb6110aef70f307131b2e3921e1c44
Pavel V <voropaev@phystech.edu>
2017-10-31 22:47:43 +0300

    task4 WIP

---
+++ exercises/04-boring-vector/BoringVector/BoringVector.Test/UnitTest1.cs
@@ -0,0 +1,49 @@
+using System;
+using Xunit;
+
+namespace BoringVector.Test
+{
+    public class UnitTest1
+    {
+        internal static bool Equals(double one, double other)
+        {
+            return Math.Abs(one - other) < VectorExtentions.Eps;
+        }
+
+        internal static bool Equals(Vector one, Vector other)
+        {
+            var xEqual = Equals(one.X, other.X);
+            var yEqual = Equals(one.Y, other.Y);
+
+            return xEqual && yEqual;
+        }
+
+        [Theory]
+        [InlineData(0, 0, "(0; 0)")]
+        [InlineData(-1, 567.4, "(-1; 567,4)")]
+        public void TestToString(double X, double Y, string result)
+        {
+            Assert.True(Equals(result, new Vector(X, Y).ToString()));
+        }
+
+
+        [Theory]
+        [InlineData(0, 0, 10, 0, 0)]
+        [InlineData(10, 10, 1, 10, 10)]
+        [InlineData(10, 10, 0, double.PositiveInfinity, double.PositiveInfinity)]
+        public void TestDivision(double X, double Y, double k, double XExpect, double YExpect)
+        {
+            Assert.True(Equals(new Vector(XExpect, YExpect), new Vector(X, Y) / k));
+        }
+
+        [Theory]
+        [InlineData(10, 1, 0, 10)]
+        public void TestAngle(double X1, double Y1, double X2, double Y2)
+        {
+            Vector v1 = new Vector(X1, Y1);
+            Vector v2 = new Vector(X2, Y2);
+            Assert.True(Equals(v1.GetAngleBetween(v2), v2.GetAngleBetween(v1)));
+        }
+
+    }
+}
--- exercises/04-boring-vector/BoringVector/BoringVector/VectorExtensions.cs
+++ exercises/04-boring-vector/BoringVector/BoringVector/VectorExtensions.cs
@@ -1,5 +1,6 @@
 ﻿namespace BoringVector
 {
+    using System;
     /*
         Здесь тебе нужно написать класс с методами-расширениями структуры Vector:
             - IsZero: проверяет, является ли вектор нулевым, т.е. его координаты близки к нулю (в эпсилон окрестности). За эпсилон здесь и далее берем 1e-6.
@@ -7,4 +8,78 @@
             - GetAngleBetween: возвращает угол между двумя векторами в радианах. Примечание: нулевой вектор сонаправлен любому другому.
             - GetRelation: возвращает значение перечесления VectorRelation(General, Parallel, Orthogonal) - отношение между двумя векторами("общий случай", параллельны, перпендикулярны). Перечисление задавать тоже тебе)
     */
+
+    /// <summary>
+    /// Виды взаиморасположения двух векторов: общее, параллельное, перпендикулярное 
+    /// </summary>
+    public enum VectorRelation
+    {
+        General,
+        Parallel,
+        Orthogonal
+    }
+
+    internal static class VectorExtentions
+    {
+        /// <summary>
+        /// Погрешность, используется для сравнения <see cref="double"/> по умолчанию в этом классе 
+        /// </summary>
+        public const double Eps = 1e-6;
+
+        /// <summary>
+        /// Является ли вектор нулевым
+        /// </summary>
+        /// <param name="v">Объект <see cref="Vector"/>, который нужно сравнить с нулевым</param>
+        /// <param name="eps">Погрешность сравнения, по умолчанию <see cref="Eps"/></param>
+        /// <returns><see cref="bool"/>, да или нет</returns>
+        internal static bool IsZero(this Vector v, double eps = Eps)
+        {
+            return Math.Abs(v.X) < eps && Math.Abs(v.Y) < eps;
+        }
+
+        /// <summary>
+        /// Нормировка вектора
+        /// </summary>
+        /// <param name="v">Объект <see cref="Vector"/>, который нужно нормировать (растянуть так, чтобы длина стала равной 1)</param>
+        /// <returns>Новый объект <see cref="Vector"/>, равный нормированному старому</returns>
+        internal static Vector Normalize(this Vector v)
+        {
+            var length = Math.Sqrt(v.SquareLength());
+            return v / length;
+        } 
+
+        /// <summary>
+        /// Угол между векторами
+        /// </summary>
+        /// <param name="one"></param>
+        /// <param name="other"></param>
+        /// <returns>Число <see cref="double"/>, равное углу (в радианах) между двумя векторами.
+        /// Углом называется кратчайший из двух вертикальных углов, образуемых прямыми, построенными на продолжении векторов.
+        /// Нулевой вектор сонаправлен любому.</returns>
+        internal static double GetAngleBetween(this Vector one, Vector other)
+        {
+            if (one.IsZero() || other.IsZero())
+                return 0;
+
+            return one.DotProduct(other) / Math.Sqrt(one.SquareLength() * other.SquareLength());
+        }
+
+        /// <summary>
+        /// Взаимное положение векторов (параллельны, перпендикулярны, в общем положении)
+        /// </summary>
+        /// <param name="one">Объект <see cref="Vector"/></param>
+        /// <param name="other">Объект <see cref="Vector"/></param>
+        /// <param name="eps">Погрешность сравнения double, по умолчанию <see cref="Eps"/></param>
+        /// <returns>Объект перечисления <see cref="VectorRelation"/>, соответствующий взаиморасположению векторов</returns>
+        internal static VectorRelation GetRelation(this Vector one, Vector other, double eps = Eps)
+        {
+            if (Math.Abs(one.GetAngleBetween(other)) < eps)
+                return VectorRelation.Parallel;
+
+            if (Math.Abs(one.DotProduct(other)) < eps)
+                return VectorRelation.Orthogonal;
+
+            return VectorRelation.General;
+        }
+    }
 }
