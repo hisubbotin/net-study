29a8a5b73ed4701ed09e5aa76246e67ec32601ec
Peter Pitovsky <Peter Pitovsky>
2017-11-03 01:34:10 +0300

    boring vector

--- exercises/04-boring-vector/BoringVector/BoringVector/VectorExtensions.cs
+++ exercises/04-boring-vector/BoringVector/BoringVector/VectorExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace BoringVector
+﻿using System;
+
+namespace BoringVector
 {
     /*
         Здесь тебе нужно написать класс с методами-расширениями структуры Vector:
@@ -7,4 +9,49 @@
             - GetAngleBetween: возвращает угол между двумя векторами в радианах. Примечание: нулевой вектор сонаправлен любому другому.
             - GetRelation: возвращает значение перечесления VectorRelation(General, Parallel, Orthogonal) - отношение между двумя векторами("общий случай", параллельны, перпендикулярны). Перечисление задавать тоже тебе)
     */
+
+    internal enum VectorRelation
+    {
+        Parallel,
+        Orthogonal,
+        General
+    }
+
+    internal static class VectorExtensions
+    {
+
+        private const double epsilon = 1e-6;
+
+        public static bool IsZero(this Vector v)
+        {
+            return v.SquareLength() < epsilon * epsilon;
+        }
+
+        public static Vector Normalize(this Vector v)
+        {
+            return v.Scale(1 / Math.Sqrt(v.SquareLength()));
+        }
+
+        public static double GetAngleBetween(this Vector a, Vector b)
+        {
+            if (a.IsZero() || b.IsZero())
+            {
+                return 0;
+            }
+            return Math.Acos(a.DotProduct(b) / (Math.Sqrt(a.SquareLength() * b.SquareLength())));
+        }
+
+        public static VectorRelation GetRelation(this Vector a, Vector b)
+        {
+            if (a.CrossProduct(b) < epsilon)
+            {
+                return VectorRelation.Parallel;
+            }
+            if (a.DotProduct(b) < epsilon)
+            {
+                return VectorRelation.Orthogonal;
+            }
+            return VectorRelation.General;
+        }
+    }
 }
