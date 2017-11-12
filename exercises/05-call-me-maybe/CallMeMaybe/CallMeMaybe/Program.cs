f37e5702da40e251f08c3967edc41aaee80fe974
Petr Kuderov <Petr_Ku@abbyy.com>
2017-11-12 19:27:13 +0300

    init

---
+++ exercises/05-call-me-maybe/CallMeMaybe/CallMeMaybe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CallMeMaybe
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
