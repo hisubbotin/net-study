29c5ad8b0d139b36185fa776e64db7bbef02c12f
Шишкин Евгений Андреевич <040796@gmail.com>
2017-11-24 23:55:02 +0300

    Maybe V1

--- exercises/05-call-me-maybe/CallMeMaybe/CallMeMaybe.V1/Maybe.cs
+++ exercises/05-call-me-maybe/CallMeMaybe/CallMeMaybe.V1/Maybe.cs
@@ -17,7 +17,9 @@ namespace CallMeMaybe.V1
         public bool HasValue { get; }
 
         private readonly T _value;
-        public T Value => HasValue ? _value : throw new InvalidOperationException($"{typeof(Maybe<T>)} doesn't have value.");
+
+        public T Value =>
+            HasValue ? _value : throw new InvalidOperationException($"{typeof(Maybe<T>)} doesn't have value.");
 
         /// <summary>
         /// Как думаешь, почему я скрыл конструктор?
@@ -40,48 +42,65 @@ namespace CallMeMaybe.V1
                 По смыслу это фабрика объектов данного типа (ну или по модному монадный конструктор).
                 Т.к. это оператор неявного приведения, позволяет не засорять код кастами.
             */
-            throw new NotImplementedException();
+            return value != null ? new Maybe<T>(value) : Maybe<T>.Nothing;
         }
 
         #region Optional useful methods
 
         public static explicit operator T(Maybe<T> maybe)
         {
-            throw new NotImplementedException();
+            return maybe.Value;
         }
 
-        public T GetValueOrDefault() => throw new NotImplementedException();
-        public T GetValueOrDefault(T defaultValue) => throw new NotImplementedException();
+        public T GetValueOrDefault() => HasValue ? _value : default(T);
+        public T GetValueOrDefault(T defaultValue) => HasValue ? _value : defaultValue;
 
         public Maybe<TResult> Select<TResult>(Func<T, TResult> map)
         {
-            throw new NotImplementedException();
+            return HasValue ? new Maybe<TResult>(map(_value)) : Maybe<TResult>.Nothing;
         }
+
         public Maybe<TResult> Select<TResult>(Func<T, Maybe<TResult>> maybeMap)
         {
-            throw new NotImplementedException();
+            return HasValue ? maybeMap(_value) : Maybe<TResult>.Nothing;
         }
+
         public TResult SelectOrElse<TResult>(Func<T, TResult> map, Func<TResult> elseMap)
         {
-            throw new NotImplementedException();
+            return HasValue ? map(_value) : elseMap();
         }
 
         public void Do(Action<T> doAction)
         {
-            throw new NotImplementedException();
-        }
-        public void DoOrElse(Action<T> doAction, Action elseAction)
-        {
-            throw new NotImplementedException();
+            if (HasValue)
+            {
+                doAction(_value);
+            }
         }
 
-        public TResult OrElse<TResult>(Func<TResult> elseMap)
+        public void DoOrElse(Action<T> doAction, Action elseAction)
         {
-            throw new NotImplementedException();
+            if (HasValue)
+            {
+                doAction(_value);
+            }
+            else
+            {
+                elseAction();
+            }
         }
+
+        public T OrElse(Func<T> elseMap)
+        {
+            return HasValue ? _value : elseMap();
+        }
+
         public void OrElseDo(Action elseAction)
         {
-            throw new NotImplementedException();
+            if (!HasValue)
+            {
+                elseAction();
+            }
         }
 
         #endregion
