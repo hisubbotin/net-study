c5c519fef6cb7afefbf6ab988f49677677477556
Данковцев Максим <max.dankow@gmail.com>
2017-11-25 20:11:08 +0300

    Комментарии

--- exercises/04-boring-vector/BoringVector/BoringVector/VectorExtensions.cs
+++ exercises/04-boring-vector/BoringVector/BoringVector/VectorExtensions.cs
@@ -19,23 +19,41 @@ namespace BoringVector
 
     internal static class VectorExtentions
     {
+        /// <summary>
+        /// Определяет допустимую разность между двумя вещественными числами, которые считаются равными.
+        /// </summary>
         private const double DefaultPrecision = 1e-6;
         
+        /// <summary>
+        /// Проверяет, что вектор <see cref="v"/> является вектором (0, 0).
+        /// </summary>
+        /// <param name="v">Проверяемый вектор.</param>
+        /// <param name="precision">Допустимую разность между двумя вещественными числами, которые считаются равными.</param>
+        /// <returns></returns>
         internal static bool IsZero(this Vector v, double precision = DefaultPrecision)
         {
             return Math.Abs(v.X) < precision && Math.Abs(v.Y) < precision;
         }
         
+        /// <summary>
+        /// Кто бы мог подумать, возвращает длину вектора <see cref="v"/>.
+        /// </summary>
         internal static double Length(this Vector v)
         {
             return Math.Sqrt(v.SquareLength());
         }
 
+        /// <summary>
+        /// Возвращает нормализованный вектор <see cref="v"/>
+        /// </summary>
         internal static Vector Normalize(this Vector v)
         {
             return v / v.Length();
         }
 
+        /// <summary>
+        /// Возвращает угол между векторами <see cref="v"/> и <see cref="u"/>.
+        /// </summary>
         internal static double GetAngleBetween(this Vector v, Vector u)
         {
             if (v.IsZero() || v.IsZero())
@@ -45,6 +63,13 @@ namespace BoringVector
             return Math.Acos( v.DotProduct(u) / ( v.Length() * u.Length() ) );
         }
 
+        /// <summary>
+        /// Проверяет как взаимно расположены векторы <see cref="v"/> и <see cref="u"/>.
+        /// </summary>
+        /// <param name="v"></param>
+        /// <param name="u"></param>
+        /// <param name="precision">Допустимую разность между двумя вещественными числами, которые считаются равными.</param>
+        /// <returns><see cref="VectorRelation"/> описывающий взаимное расположение</returns>
         internal static VectorRelation GetRelation(this Vector v, Vector u, double precision = DefaultPrecision)
         {
             if (Math.Abs(v.DotProduct(u)) < precision)
