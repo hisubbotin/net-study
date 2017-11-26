00b0040074082bf1df1f0909ff1e1707abf63e2d
Данковцев Максим <max.dankow@gmail.com>
2017-11-26 22:44:03 +0300

    Task Drunk Fibonacci done

--- exercises/03-drunk-fibonacci/DrunkFibonacci/DrunkFibonacci/Program.cs
+++ exercises/03-drunk-fibonacci/DrunkFibonacci/DrunkFibonacci/Program.cs
@@ -1,14 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DrunkFibonacci
 {
     internal class Program
     {
+        private static void Describe<T>(string message, IEnumerable<T> data, int count = 10)
+        {
+            Console.WriteLine(message + ":");
+            Console.Write(string.Join(", ", data.Take(count)));
+            if (data.Skip(count).Any())
+            {
+                Console.WriteLine("...");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+        }
+        
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
-
+            Describe("Deterministic Random", DrunkFibonacci.GetDeterministicRandomSequence());
+            Describe("Drunk Values", DrunkFibonacci.GetDrunkFibonacci(), 20);
+            List<int[]> buckets = DrunkFibonacci.GetInChunks().Take(3).ToList();
+            foreach (var bucket in buckets)
+            {
+                Describe("Drunk Values in chunks", bucket, 16);
+            }
+            
             Console.WriteLine("GetDrunkFibonacci: " + string.Join(',', DrunkFibonacci.GetDrunkFibonacci().Take(100)));
             Console.WriteLine("GetMaxOnRange: " + DrunkFibonacci.GetMaxOnRange(1000, 400));
             Console.WriteLine("GetNextNegativeRange: " + string.Join(',', DrunkFibonacci.GetNextNegativeRange().Take(100)));
