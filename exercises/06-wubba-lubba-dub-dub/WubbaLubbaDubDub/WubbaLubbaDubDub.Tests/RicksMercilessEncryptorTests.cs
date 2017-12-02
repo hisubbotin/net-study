5d49760590423282753ea72364c94e9d1a831467
Шишкин Евгений Андреевич <040796@gmail.com>
2017-12-02 18:18:12 +0300

    Сделал задание до конца

--- exercises/06-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
+++ exercises/06-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Data.Common;
 using System.IO;
+using System.Linq;
 using System.Reflection;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using Xunit;
 using Xunit.Sdk;
 
@@ -51,9 +49,141 @@ namespace WubbaLubbaDubDub.Tests
     {
         [Theory]
         [TextFileData("Data/Text.txt")]
+        public void Test_TextFileData(string text)
+        {
+            Assert.Equal(text, "Hello, World!");
+        }
+
+        [Theory]
+        [TextFileData("Data/BigText.txt")]
         public void Test_SplitToLines(string text)
         {
-            Assert.True(text == "Hello, World!");
+            Assert.Equal(text.SplitToLines().Length, 40);
+        }
+
+        [Theory]
+        [InlineData(
+            "Мама мыла раму",
+            new string[] {"Мама", "мыла", "раму"}
+        )]
+        [InlineData(
+            "Мама, а ты мыла раму?",
+            new string[] {"Мама", "а", "ты", "мыла", "раму"}
+        )]
+        public void Test_SplitToWords(string line, string[] result)
+        {
+            Assert.Equal(line.SplitToWords(), result);
+        }
+
+        [Theory]
+        [InlineData(
+            "abce",
+            "ab"
+        )]
+        [InlineData(
+            "abcde",
+            "ab"
+        )]
+        [InlineData(
+            "Мама мыла раму",
+            "Мама мы"
+        )]
+        public void Test_GetLeftHalf(string line, string result)
+        {
+            Assert.Equal(line.GetLeftHalf(), result);
+        }
+
+        [Theory]
+        [InlineData(
+            "abce",
+            "ce"
+        )]
+        [InlineData(
+            "abcde",
+            "cde"
+        )]
+        [InlineData(
+            "Мама мыла раму",
+            "ла раму"
+        )]
+        public void Test_GetRightHalf(string line, string result)
+        {
+            Assert.Equal(line.GetRightHalf(), result);
+        }
+
+        [Theory]
+        [InlineData(
+            "Мама мыла раму",
+            "ам",
+            "ап",
+            "Мапа мыла рапу"
+        )]
+        public void Test_Replace(string line, string old, string replaceWith, string result)
+        {
+            Assert.Equal(line.Replace(old, replaceWith), result);
+        }
+
+        [Theory]
+        [InlineData(
+            "A",
+            "\\u0041"
+        )]
+        [InlineData(
+            "Мама мыла раму",
+            "\\u041C\\u0430\\u043C\\u0430\\u0020\\u043C\\u044B\\u043B\\u0430\\u0020\\u0440\\u0430\\u043C\\u0443"
+        )]
+        public void Test_CharsToCodes(string line, string result)
+        {
+            Assert.Equal(line.CharsToCodes(), result);
+        }
+
+        [Theory]
+        [InlineData(
+            "abce",
+            "ecba"
+        )]
+        [InlineData(
+            "Мама мыла раму",
+            "умар алым амаМ"
+        )]
+        public void Test_GetReversed(string line, string result)
+        {
+            Assert.Equal(line.GetReversed(), result);
+        }
+
+        [Theory]
+        [InlineData(
+            "abce 43",
+            "ABCE 43"
+        )]
+        [InlineData(
+            "Мама мыла раму",
+            "мАМА МЫЛА РАМУ"
+        )]
+        public void Test_InverseCase(string line, string result)
+        {
+            Assert.Equal(line.InverseCase(), result);
+        }
+
+        [Theory]
+        [InlineData(
+            "abce 43",
+            "bcdf!54"
+        )]
+        [InlineData(
+            "Мама мыла раму",
+            "Нбнб!ньмб!сбнф"
+        )]
+        public void Test_ShiftInc(string line, string result)
+        {
+            Assert.Equal(line.ShiftInc(), result);
+        }
+
+        [Theory]
+        [TextFileData("Data/IdText.txt")]
+        public void Test_GetUsedObjects(string text)
+        {
+            Assert.Equal(text.GetUsedObjects(), new long[] {4038984096L, 4037935265L});
         }
     }
 }
--- exercises/06-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub/RicksMercilessEncryptor.cs
+++ exercises/06-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub/RicksMercilessEncryptor.cs
@@ -23,7 +23,7 @@ namespace WubbaLubbaDubDub
         public static string[] SplitToWords(this string line)
         {
             // А вот здесь поиграйся с регулярками.
-            return new Regex("\\w").Matches(line).Select((match => match.Groups[0].Value)).ToArray();
+            return new Regex("\\w+").Matches(line).Select(match => match.Groups[0].Value).ToArray();
         }
 
         /// <summary>
@@ -33,7 +33,7 @@ namespace WubbaLubbaDubDub
         public static string GetLeftHalf(this string s)
         {
             // у строки есть метод получения подстроки
-            throw new NotImplementedException();
+            return s.Substring(0, s.Length / 2);
         }
 
         /// <summary>
@@ -42,7 +42,7 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string GetRightHalf(this string s)
         {
-            throw new NotImplementedException();
+            return s.Substring(s.Length / 2);
         }
 
         /// <summary>
@@ -51,7 +51,7 @@ namespace WubbaLubbaDubDub
         public static string Replace(this string s, string old, string @new)
         {
             // и такой метод у строки, очевидно, тоже есть
-            throw new NotImplementedException();
+            return s.Replace(old, @new);
         }
 
         /// <summary>
@@ -67,7 +67,7 @@ namespace WubbaLubbaDubDub
                 FYI: локальную функцию можно объявлять даже после строки с return.
                 То же самое можно сделать и для всех оставшихся методов.
             */
-            throw new NotImplementedException();
+            return string.Join(string.Empty, s.Select(c => $"\\u{Convert.ToInt32(c):X4}"));
         }
 
         /// <summary>
@@ -79,7 +79,7 @@ namespace WubbaLubbaDubDub
                 Собрать строку из последовательности строк можно несколькими способами.
                 Один из низ - статический метод Concat. Но ты можешь выбрать любой.
             */
-            throw new NotImplementedException();
+            return string.Join(string.Empty, s.Reverse());
         }
 
         /// <summary>
@@ -92,7 +92,12 @@ namespace WubbaLubbaDubDub
                 На минуту задержись здесь и посмотри, какие еще есть статические методы у char.
                 Например, он содержит методы-предикаты для определения категории Юникода символа, что очень удобно.
             */
-            throw new NotImplementedException();
+            char InverseCase(char c)
+            {
+                return char.IsLower(c) ? char.ToUpper(c) : char.ToLower(c);
+            }
+
+            return string.Join(string.Empty, s.Select(InverseCase));
         }
 
         /// <summary>
@@ -101,7 +106,7 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string ShiftInc(this string s)
         {
-            throw new NotImplementedException();
+            return string.Join(string.Empty, s.Select(c => $"{char.ConvertFromUtf32(Convert.ToInt32(c) + 1)}"));
         }
 
 
@@ -119,7 +124,11 @@ namespace WubbaLubbaDubDub
                 Задача на поиграться с регулярками - вся сложность в том, чтобы аккуратно игнорировать комментарии.
                 Экспериментировать онлайн можно, например, здесь: http://regexstorm.net/tester и https://regexr.com/
             */
-            throw new NotImplementedException();
+            var idRegex = new Regex("[0-9A-F]{4}:[0-9A-F]{4}");
+            var commentRegex = new Regex("\\/\\/.*\\n|\\/\\*(.|\\n)*?\\*\\/");
+            return commentRegex.Split(text)
+                .SelectMany(s => idRegex.Matches(s).Select(match => match.Groups[0].Value))
+                .Select(s => Convert.ToInt64(s.Replace(":", string.Empty), 16)).ToImmutableList();
         }
 
         #endregion
