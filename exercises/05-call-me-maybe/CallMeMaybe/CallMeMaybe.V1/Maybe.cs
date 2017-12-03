6aef8b8c5b8b94f176bacfe433c16cea58e624ed
Данковцев Максим <max.dankow@gmail.com>
2017-12-03 20:38:32 +0300

    Maybe implemented (maybe)

--- exercises/05-call-me-maybe/CallMeMaybe/CallMeMaybe.V1/Maybe.cs
+++ exercises/05-call-me-maybe/CallMeMaybe/CallMeMaybe.V1/Maybe.cs
@@ -2,22 +2,30 @@
 
 namespace CallMeMaybe.V1
 {
-    public struct Maybe<T>
+    /// <summary>
+    /// Представляет универсальную обертку над ссылочным (в отличие от <see cref="System.Nullable"/>) или значимым типом, 
+    /// которая может содержать или не содержать значение. 
+    /// </summary>
+    /// <typeparam name="T">Тип содержимого</typeparam>
+    public struct Maybe<T> 
     {
         /*
-            Как ты думаешь, почему Maybe - структура?
+            Maybe - структура, так как если бы это был ссылочный тип, то он сам мог бы быть null
+            и пришлось бы и его самого проверять на null, что свело бы на нет все удобство Maybe. 
         */
 
         /// <summary>
-        /// Зачем может быть нужно такое выделенное значение?
-        /// Сколько по факту будет экземпляров данного объекта?
+        /// Выделенное значение для сравнения с Maybe, не содержащим значения.
+        /// Объект будет создан один на весь класс.
         /// </summary>
         public static readonly Maybe<T> Nothing = new Maybe<T>();
 
         public bool HasValue { get; }
 
         private readonly T _value;
-        public T Value => HasValue ? _value : throw new InvalidOperationException($"{typeof(Maybe<T>)} doesn't have value.");
+
+        public T Value =>
+            HasValue ? _value : throw new InvalidOperationException($"{typeof(Maybe<T>)} doesn't have value.");
 
         /// <summary>
         /// Как думаешь, почему я скрыл конструктор?
@@ -40,48 +48,65 @@ namespace CallMeMaybe.V1
                 По смыслу это фабрика объектов данного типа (ну или по модному монадный конструктор).
                 Т.к. это оператор неявного приведения, позволяет не засорять код кастами.
             */
-            throw new NotImplementedException();
+            return value == null ? Nothing : new Maybe<T>(value);
         }
 
         #region Optional useful methods
 
         public static explicit operator T(Maybe<T> maybe)
         {
-            throw new NotImplementedException();
+            return maybe.Value;
         }
 
-        public T GetValueOrDefault() => throw new NotImplementedException();
-        public T GetValueOrDefault(T defaultValue) => throw new NotImplementedException();
+        public T GetValueOrDefault() => GetValueOrDefault(default(T));
+        public T GetValueOrDefault(T defaultValue) => HasValue ? _value : defaultValue;
 
         public Maybe<TResult> Select<TResult>(Func<T, TResult> map)
         {
-            throw new NotImplementedException();
+            return HasValue ? map(_value) : Maybe<TResult>.Nothing;
         }
+
         public Maybe<TResult> Select<TResult>(Func<T, Maybe<TResult>> maybeMap)
         {
-            throw new NotImplementedException();
+            return HasValue ? maybeMap(_value) : Maybe<TResult>.Nothing;
         }
+
         public TResult SelectOrElse<TResult>(Func<T, TResult> map, Func<TResult> elseMap)
         {
-            throw new NotImplementedException();
+            return HasValue ? map(_value) : elseMap();
         }
 
         public void Do(Action<T> doAction)
         {
-            throw new NotImplementedException();
+            if (HasValue)
+            {
+                doAction(_value);
+            }
         }
+
         public void DoOrElse(Action<T> doAction, Action elseAction)
         {
-            throw new NotImplementedException();
+            if (HasValue)
+            {
+                doAction(_value);
+            }
+            else
+            {
+                elseAction();
+            }
         }
 
         public T OrElse(Func<T> elseMap)
         {
-            throw new NotImplementedException();
+            return HasValue ? _value : elseMap();
         }
+
         public void OrElseDo(Action elseAction)
         {
-            throw new NotImplementedException();
+            if (!HasValue)
+            {
+                elseAction();
+            }
         }
 
         #endregion
--- exercises/05-call-me-maybe/CallMeMaybe/CallMeMaybe.V1/MaybeExtensions.cs
+++ exercises/05-call-me-maybe/CallMeMaybe/CallMeMaybe.V1/MaybeExtensions.cs
@@ -2,8 +2,14 @@
 {
     public static class MaybeExtensions
     {
-        /*
-            здесь мог бы быть твой код
-        */
+        public static Maybe<T> ToMaybe<T>(this T value)
+        {
+            return value;
+        }
+        
+        public static Maybe<T> ToMaybe<T>(this T? nullable) where T : struct
+        {
+            return nullable ?? Maybe<T>.Nothing;
+        }
     }
 }
