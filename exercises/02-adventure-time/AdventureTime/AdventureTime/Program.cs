cd3534c58aeba060f7087b1c79218f52634bdd51
cheshire <NikitaSerenko@users.noreply.github.com>
2017-12-05 17:01:32 +0300

    Update Program.cs

--- exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -1,16 +1,9 @@
-﻿using System;
 namespace AdventureTime
 {
     internal class Program
     {
         private static void Main()
         {
-            DateTime d = DateTime.Now;
-            DateTime s = d.AddMonths(1);
-            System.Console.WriteLine(Time.AreEqualBirthdays(d, d));
-            System.Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
-            System.Console.WriteLine(Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
-            System.Console.ReadLine();
         }
     }
 }
