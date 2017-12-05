e253e3334a7f2bb8c61845cca5ce1ea5d698419a
Daria Zvereva <zverevads@gmail.com>
2017-12-05 15:03:46 +0300

    Ready

---
+++ exercises/06-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaMain/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Immutable;
+using WubbaLubbaDubDub;
+
+namespace WubbaMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = "//Barselooona \n /* dfjhdk \n skjfhk AAAA:5555 \n */ BBBB:4444";
+            //Console.WriteLine(line);
+            IImmutableList<long> ans = line.GetUsedObjects().ToImmutableArray();
+            Console.WriteLine(ans[0]);
+            Console.WriteLine("FSGA");
+            Console.ReadKey();
+        }
+    }
+}
