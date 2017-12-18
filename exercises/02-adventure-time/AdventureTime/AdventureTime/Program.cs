36b36aa32f65a7c2c141b4b9029e84357ce4e0b5
IrinaGavenko <rina9159@mail.ru>
2017-12-18 15:23:07 +0300

    task2

--- exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace AdventureTime
+﻿using System;
+namespace AdventureTime
 {
     internal class Program
     {
         private static void Main()
         {
+            // эта вещь мне не понравилась, пришлось проверить
+            Console.WriteLine("3 months: " + Time.GetTotalMinutesInThreeMonths());
+            
+            // а вот это, действительно, интересно
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+            Console.WriteLine(Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+            
+            var bd1 = new DateTime(1992, 10, 30, 11, 37, 0);
+            var bd2 = new DateTime(1996, 7, 27, 16, 29, 0);
+            Console.WriteLine(Time.AreEqualBirthdays(bd1, bd2));
+            
+            Console.ReadKey();
         }
     }
 }
