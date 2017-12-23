994ce586b4c3bbf917a8f762d23116b50db9927c
kkvadrat289 <kkvadrat289@yandex.ru>
2017-12-24 01:20:09 +0300

    tests

---
+++ exercises/04-boring-vector/BoringVector/BoringVector.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BoringVector.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BoringVector.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("65a7b647-b446-4bf7-b9ee-7db0b72530f7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ exercises/04-boring-vector/BoringVector/BoringVector.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+
+namespace BoringVector.Tests
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData(3,4,25)]
+        public void SquareLengthTest(double X, double Y, double res)
+        {
+           
+        }
+    }
+}
