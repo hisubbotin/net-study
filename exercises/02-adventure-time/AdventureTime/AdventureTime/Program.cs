774647b0094aeb086b5a9a0b5431a586d829b91f
Евгений Суханов < >
2018-01-25 02:45:56 +0300

    ex2

--- exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -4,6 +4,17 @@
     {
         private static void Main()
         {
+            System.Console.WriteLine(Time.WhatTimeIsItInUtc().TimeOfDay);
+            System.Console.WriteLine(Time.SpecifyKind(Time.WhatTimeIsIt(), System.DateTimeKind.Utc));
+            System.Console.WriteLine(Time.ToRoundTripFormatString(Time.SpecifyKind(Time.WhatTimeIsIt(), System.DateTimeKind.Utc)));
+
+            System.Console.WriteLine(Time.GetTotalMinutesInThreeMonths());
+            System.Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver0_Dumb());
+            System.Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver1_FeelsSmarter());
+            System.Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver3_NodaTime());
+            System.Console.WriteLine(Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver0_Dumb());
+            System.Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+            
         }
     }
 }
