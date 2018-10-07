fa61f9254712ade50870c9a067e1947159831ce1
Дмитрий Родин <rodin.dv@phystech.edu>
2018-10-07 15:57:31 +0300

    task 2

---
+++ course-2018-1/exercises/02-adventure-time/AdventureTime/AdventureTime.Test/AdventureTime.Test/UnitTest1.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Globalization;
+using Xunit;
+
+namespace AdventureTime.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CurrentIsNotUtc()
+        {
+            DateTime current = Time.WhatTimeIsIt();
+            DateTime utc = Time.WhatTimeIsItInUtc();
+            Assert.NotEqual(current, utc);
+        }
+
+        [Fact]
+        public void IsConvertible()
+        {
+            DateTime current = Time.WhatTimeIsIt();
+            String currentString = Time.ToRoundTripFormatString(current);
+            Assert.Equal(current, Time.ParseFromRoundTripFormat(currentString));
+        }
+
+        [Fact]
+        public void ToUTCConversion()
+        {
+            DateTime time = DateTime.Now;
+            DateTime utc = Time.ToUtc(time);
+            Assert.Equal(utc , time - TimeSpan.FromHours(3));
+        }
+
+        [Fact]
+        public void TestHours()
+        {
+            DateTime dt1 = new DateTime(2010, 3, 5, 12, 0, 0);
+            DateTime dt2 = new DateTime(2010, 3, 5, 14, 0, 0);
+            Assert.Equal(0, Time.GetHoursBetween(dt1, dt1));
+            Assert.Equal(2, Time.GetHoursBetween(dt1, dt2));
+        }
+
+        [Fact]
+        public void AdventureTimeTest()
+        {
+            Assert.Equal(180, Time.GetAdventureTimeDurationInMinutes_ver0_Dumb());
+            Assert.Equal(60, Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver0_Dumb());
+            Assert.Equal(180, Time.GetAdventureTimeDurationInMinutes_ver1_FeelsSmarter());
+            Assert.Equal(120, Time.GetAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+            Assert.Equal(120, Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+        }
+    }
+}
--- course-2018-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ course-2018-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace AdventureTime
+﻿
+namespace AdventureTime
 {
     internal class Program
     {
