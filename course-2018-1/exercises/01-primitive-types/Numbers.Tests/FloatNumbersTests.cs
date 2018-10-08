f138ea9f9c3a3453d54a2ed50b03964a41e418f7
Unknown <K_G_V97@mail.ru>
2018-10-08 03:35:46 +0300

    [ex01- solved] Krylov Gregory

--- course-2018-1/exercises/01-primitive-types/Numbers.Tests/FloatNumbersTests.cs
+++ course-2018-1/exercises/01-primitive-types/Numbers.Tests/FloatNumbersTests.cs
@@ -21,5 +21,14 @@ namespace Numbers.Tests
         {
             Assert.True(FloatNumbers.IsNaN(FloatNumbers.GetNaN()));
         }
+
+        [Theory]
+        [InlineData(1.0, 1.0000000001, 0)]
+        [InlineData(1.0, 2.0, -1)]
+        [InlineData(2.0, 1.0, 1)]
+        public void Test_Compare(double a, double b, int result)
+        {
+            Assert.Equal(FloatNumbers.Compare(a, b), result);
+        }
     }
 }
