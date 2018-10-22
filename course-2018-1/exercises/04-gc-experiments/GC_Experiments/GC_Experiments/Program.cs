398298c0ba567381ecc7d61a1292c164722f049a
iksanov <iksanov@yahoo.com>
2018-10-22 04:22:00 +0300

    finish ex4

---
+++ course-2018-1/exercises/04-gc-experiments/GC_Experiments/GC_Experiments/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GC_Experiments
+{
+    class Program
+    {
+        private static void Generations()
+        {
+            var stopwatch = new Stopwatch();
+            
+            var exp_arr = new double[10000];
+            
+            stopwatch.Start();
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.WaitForPendingFinalizers();
+            stopwatch.Stop();
+            Console.WriteLine("Generation 0: " + stopwatch.Elapsed);
+
+            stopwatch.Restart();
+            GC.Collect(1, GCCollectionMode.Forced);
+            GC.WaitForPendingFinalizers();
+            stopwatch.Stop();
+            Console.WriteLine("Generation 1: " + stopwatch.Elapsed);
+            
+            stopwatch.Restart();
+            GC.Collect(2, GCCollectionMode.Forced);
+            GC.WaitForPendingFinalizers();
+            stopwatch.Stop();
+            Console.WriteLine("Generation 2: " + stopwatch.Elapsed);
+        }
+        
+        static void Main(string[] args)
+        {
+//            Generations();
+           
+            var str = "";
+            while (GC.GetGeneration(str) != 2)
+            {
+                str += "a";
+            }
+            Console.WriteLine("Strings: " + str.Length);
+
+            var intArrMaxSize = 0;
+            var intArr = new int[intArrMaxSize];
+            while (GC.GetGeneration(intArr) != 2)
+            {
+                intArr = new int[++intArrMaxSize];
+            }
+            Console.WriteLine("Int arrays: " + intArrMaxSize);
+            
+            var doubleArrMaxSize = 0;
+            var doubleArr = new double[doubleArrMaxSize];
+            while (GC.GetGeneration(doubleArr) != 2)
+            {
+                doubleArr = new double[++doubleArrMaxSize];
+            }
+            Console.WriteLine("Double arrays: " + doubleArrMaxSize);
+        }
+    }
+}
