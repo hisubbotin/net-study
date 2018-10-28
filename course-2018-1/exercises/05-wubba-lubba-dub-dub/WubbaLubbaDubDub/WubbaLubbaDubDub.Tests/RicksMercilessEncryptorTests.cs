d6d11314f92c29cc23bb31886451d1e9d84b579f
Elena Smirnova <smirnova.es@phystech.edu>
2018-10-28 22:17:53 +0300

    task 5 added

--- course-2018-1/exercises/05-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
+++ course-2018-1/exercises/05-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
@@ -5,6 +5,82 @@ namespace WubbaLubbaDubDub.Tests
 {
     public class RicksMercilessEncryptorTests
     {
-        
+        [Theory]
+        [InlineData("a a a\n b b b ", new[] {"a a a", " b b b "})]
+        [InlineData("It's line one\nIt's line two", new [] {"It's line one", "It's line two"})]
+        public void Test_SplitToLines(string s, string[] result)
+        {
+            Assert.Equal(result, s.SplitToLines());
+        }
+
+        [Theory]
+        [InlineData("a aa\nb  bb \t bbb", new[] {"a", "aa", "b", "bb", "bbb"})]
+        [InlineData("firstword second\nthird\tforth", new[] {"firstword", "second", "third", "forth"})]
+        public void Test_SplitToWords(string s, string[] result)
+        {
+            Assert.Equal(result, s.SplitToWords());
+        }
+
+        [Theory]
+        [InlineData("abcd", "ab")]
+        [InlineData("abcde", "ab")]
+        public void Test_GetLeftHalf(string s, string result)
+        {
+            Assert.Equal(result, s.GetLeftHalf());
+        }
+
+        [Theory]
+        [InlineData("abcd", "cd")]
+        [InlineData("abcde", "cde")]
+        public void Test_GetRightHalf(string s, string result)
+        {
+            Assert.Equal(result, s.GetRightHalf());
+        }
+
+        [Theory]
+        [InlineData("abcd", "b", "k", "akcd")]
+        [InlineData("abcde", "cde", "erty", "aberty")]
+        public void Test_Replace(string s, string old, string @new, string result)
+        {
+            Assert.Equal(result, RicksMercilessEncryptor.Replace(s, old, @new));
+        }
+
+        [Theory]
+        [InlineData("abc", "\\u0061\\u0062\\u0063")]
+        public void Test_CharsToCodes(string s, string result)
+        {
+            Assert.Equal(result, s.CharsToCodes());
+        }
+
+        [Theory]
+        [InlineData("abc", "cba")]
+        public void Test_GetReversed(string s, string result)
+        {
+            Assert.Equal(result, s.GetReversed());
+        }
+
+        [Theory]
+        [InlineData("aBc", "AbC")]
+        public void Test_InverseCase(string s, string result)
+        {
+            Assert.Equal(result, s.InverseCase());
+        }
+
+        [Theory]
+        [InlineData("abc", "bcd")]
+        public void Test_ShiftInc(string s, string result)
+        {
+            Assert.Equal(result, s.ShiftInc());
+        }
+
+        [Theory]
+        [InlineData("//comment ¶0000:0150¶ \n not comment ¶0000:0000¶", new[] {0L})]
+        [InlineData("0000:0012 <- thats not it ¶0000:0000¶ thats better /* multiline-line 0000:0001 \n 0000:0002 */", new long[] { 0L })]
+        [InlineData("/* comment again ¶0000:0010¶\n ¶0000:0130¶ */ ¶0000:0000¶ ", new[] {0L})]
+        public void Test_GetUsedObjects(string s, long[] result)
+        {
+            Assert.Equal(result, s.GetUsedObjects());
+        }
+
     }
 }
---
+++ course-2018-1/exercises/05-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub/BuildersComparison.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace WubbaLubbaDubDub
+{
+    internal class BuildersComparison
+    {
+        private static void Main()
+        {
+            var numOfIterations = 1000000;
+            var charArray = new char[numOfIterations];
+            for (int i = 0; i < numOfIterations; i++)
+            {
+                charArray[i] = 'a';
+            }
+
+            var sw = Stopwatch.StartNew();
+            string.Join(' ', charArray);
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+
+            sw = Stopwatch.StartNew();
+            StringBuilder sb = new StringBuilder();
+            sb.Append(charArray);
+            Console.WriteLine(sw.Elapsed);
+
+            sw = Stopwatch.StartNew();
+            string.Concat(charArray);
+            sw.Stop();
+            Console.WriteLine(sw.Elapsed);
+
+            //получаем следующие результаты:
+            // string.Join -- 00:00:00.1125908
+            // StringBuilder -- 00:00:00.0055353
+            // string.Concat -- 00:00:00.0761168
+
+            Console.Read();
+        }
+    }
+}
