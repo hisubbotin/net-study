c1a7a11db1f426898ded77abacdd5b2f242d22ca
Clumpy Tuna <anuhinm@mail.ru>
2018-10-28 21:15:59 +0300

    Task 5 added

--- course-2018-1/exercises/05-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
+++ course-2018-1/exercises/05-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
@@ -5,6 +5,64 @@ namespace WubbaLubbaDubDub.Tests
 {
     public class RicksMercilessEncryptorTests
     {
+        [Theory]
+        [InlineData("Text Text Text Text\nText Text Text\nText Text\nText", 
+            new [] {"Text Text Text Text", "Text Text Text", "Text Text", "Text" })]
+        public void TestSplitToLines(string text, string[] result)
+        {
+            Assert.Equal(text.SplitToLines(), result);
+        }
         
+        [Theory]
+        [InlineData("Text Text \n Text",
+            new[] { "Text", "Text", "Text"})]
+        public void TestSplitToWords(string line, string[] res)
+        {
+            Assert.Equal(line.SplitToWords(), res);
+        }
+        
+        [Theory]
+        [InlineData("123456789", "1234")]
+        [InlineData("12345678", "1234")]
+        public void TestGetLeftHalf(string s, string res)
+        {
+            Assert.Equal(s.GetLeftHalf(), res);
+        }
+        
+        [Theory]
+        [InlineData("123456789", "56789")]
+        [InlineData("12345678", "5678")]
+        public void TestGetRightHalf(string s, string res)
+        {
+            Assert.Equal(s.GetRightHalf(), res);
+        }
+        
+        [Theory]
+        [InlineData("123 456", "123", "456", "456 456")]
+        public void TestReplace(string s, string old, string @new, string res)
+        {
+            Assert.Equal(s.Replace(old, @new), res);
+        }
+        
+        [Theory]
+        [InlineData("123", "\\u0031\\u0032\\u0033")]
+        public void TestCharsToCodes(string s, string res)
+        {
+            Assert.Equal(s.CharsToCodes(), res);
+        }
+        
+        [Theory]
+        [InlineData("123", "321")]
+        public void TestGetReversed(string s, string res)
+        {
+            Assert.Equal(s.GetReversed(), res);
+        }
+
+        [Theory]
+        [InlineData("123", "234")]
+        public void TestShiftInc(string s, string res)
+        {
+            Assert.Equal(s.ShiftInc(), res);
+        }
     }
 }
--- course-2018-1/exercises/05-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub/RicksMercilessEncryptor.cs
+++ course-2018-1/exercises/05-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub/RicksMercilessEncryptor.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace WubbaLubbaDubDub
 {
@@ -12,7 +14,7 @@ namespace WubbaLubbaDubDub
         public static string[] SplitToLines(this string text)
         {
             // У строки есть специальный метод. Давай здесь без регулярок
-            throw new NotImplementedException();
+            return text.Split('\n');
         }
 
         /// <summary>
@@ -21,7 +23,7 @@ namespace WubbaLubbaDubDub
         public static string[] SplitToWords(this string line)
         {
             // А вот здесь поиграйся с регулярками.
-            throw new NotImplementedException();
+            return Regex.Split(line, "[^a-zA-Z]+");
         }
 
         /// <summary>
@@ -31,7 +33,7 @@ namespace WubbaLubbaDubDub
         public static string GetLeftHalf(this string s)
         {
             // у строки есть метод получения подстроки
-            throw new NotImplementedException();
+            return s.Substring(0, s.Length / 2);
         }
 
         /// <summary>
@@ -40,7 +42,7 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string GetRightHalf(this string s)
         {
-            throw new NotImplementedException();
+            return s.Substring(s.Length / 2);
         }
 
         /// <summary>
@@ -49,9 +51,15 @@ namespace WubbaLubbaDubDub
         public static string Replace(this string s, string old, string @new)
         {
             // и такой метод у строки, очевидно, тоже есть
-            throw new NotImplementedException();
+            return s.Replace(old, @new);
         }
 
+        private static string CharToUnicodeCode(char character)
+        {
+            return $"\\u{Convert.ToInt32(character):X4}";
+        }
+        
+        
         /// <summary>
         /// Возвращает строку, у которой каждый символ заменен на \uFFFF,
         /// где FFFF - соответствующая шестнадцатиричная кодовая точка.
@@ -65,7 +73,7 @@ namespace WubbaLubbaDubDub
                 FYI: локальную функцию можно объявлять даже после строки с return.
                 То же самое можно сделать и для всех оставшихся методов.
             */
-            throw new NotImplementedException();
+            return String.Concat(s.Select(CharToUnicodeCode));
         }
 
         /// <summary>
@@ -77,9 +85,22 @@ namespace WubbaLubbaDubDub
                 Собрать строку из последовательности строк можно несколькими способами.
                 Один из низ - статический метод Concat. Но ты можешь выбрать любой.
             */
-            throw new NotImplementedException();
+            return String.Concat(s.Reverse());
         }
 
+        private static char InverseCharRegister(char character)
+        {
+            if (char.IsLower(character))
+            {
+                return char.ToUpper(character);
+            }
+            else
+            {
+                return char.ToLower(character);
+            }
+        }
+        
+        
         /// <summary>
         /// Возвращает строку, у которой регистр букв заменён на противоположный.
         /// </summary>
@@ -90,16 +111,23 @@ namespace WubbaLubbaDubDub
                 На минуту задержись здесь и посмотри, какие еще есть статические методы у char.
                 Например, он содержит методы-предикаты для определения категории Юникода символа, что очень удобно.
             */
-            throw new NotImplementedException();
+
+            return string.Concat(s.Select(InverseCharRegister));
         }
 
+        private static string GetNextUnicode(char character)
+        {
+            return char.ConvertFromUtf32(Convert.ToInt32(character) + 1);
+        }
+        
+        
         /// <summary>
         /// Возвращает строку, у которой каждый символ заменен на следующий за ним символ Юникода.
         /// Т.е. каждый символ с кодовой точкой X заменен на символ с кодовой точкой X+1.
         /// </summary>
         public static string ShiftInc(this string s)
         {
-            throw new NotImplementedException();
+            return String.Concat(s.Select(GetNextUnicode));
         }
 
 
