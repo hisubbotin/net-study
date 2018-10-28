2263c5832c1db5668edb20d72e7c7c536bbda62b
kozlovanadya <nadysha1997@yandex.ru>
2018-10-28 16:42:35 +0300

    Add comparator

---
+++ course-2018-1/exercises/05-wubba-lubba-dub-dub/WubbaLubbaDubDub/ComparatorStringCreating/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+
+namespace ComparatorStringCreating
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            string s = string.Join("", "fdsfaa ", "fdsafdsafsdagagasdgfdgfdga", "fdsgdsagfgfsdddddgfsd", " dsfafagfgsdatretthf");
+            sw.Stop();
+            Console.WriteLine("string.Join: {0} ms", sw.Elapsed.TotalMilliseconds);
+
+            sw.Restart();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendJoin("fdsfaa ", "fdsafdsafsdagagasdgfdgfdga", "fdsgdsagfgfsdddddgfsd", " dsfafagfgsdatretthf");
+            string str = sb.ToString();
+            sw.Stop();
+            Console.WriteLine("StringBuilder: {0} ms", sw.Elapsed.TotalMilliseconds);
+
+            sw.Restart();
+            string s1 = "fdsfaa " + "fdsafdsafsdagagasdgfdgfdga" + "fdsgdsagfgfsdddddgfsd" + " dsfafagfgsdatretthf";
+            sw.Stop();
+            Console.WriteLine("Concatenation: {0} ms", sw.Elapsed.TotalMilliseconds);
+        }
+    }
+}
