27d84e64dddceec6f54a4cbddaba90e0b0c434ee
kozlovanadya <nadysha1997@yandex.ru>
2018-10-28 13:07:57 +0300

    task 5 WubbaLubbaDubDub

--- course-2018-1/exercises/05-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
+++ course-2018-1/exercises/05-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
@@ -1,10 +1,95 @@
 using System;
+using System.Collections.Immutable;
 using Xunit;
 
 namespace WubbaLubbaDubDub.Tests
 {
     public class RicksMercilessEncryptorTests
     {
-        
+        [Theory]
+        [InlineData("Hi\r\nThis is one\r\nBla bla bla\r\naaaaaaaaaa", 
+            new [] {"Hi", "This is one", "Bla bla bla", "aaaaaaaaaa" })]
+        public void TestSplitToLines(string text, string[] res)
+        {
+            Assert.Equal(text.SplitToLines(), res);
+        }
+
+        [Theory]
+        [InlineData("Hi This, is!! one\r\nBla bla bla\r\naaaaaaaaaa",
+            new[] { "Hi", "This", "is", "one", "Bla", "bla", "bla", "aaaaaaaaaa" })]
+        public void TestSplitToWords(string line, string[] res)
+        {
+            Assert.Equal(line.SplitToWords(), res);
+        }
+
+        [Theory]
+        [InlineData("Hi, my name is tru-la-la", "Hi, my name ")]
+        [InlineData("Hi, my name is tru-la-laa", "Hi, my name ")]
+        public void TestGetLeftHalf(string s, string res)
+        {
+            Assert.Equal(s.GetLeftHalf(), res);
+        }
+
+        [Theory]
+        [InlineData("Hi, my name is tru-la-la", "is tru-la-la")]
+        [InlineData("Hi, my name is tru-la-laa", "is tru-la-laa")]
+        public void TestGetRightHalf(string s, string res)
+        {
+            Assert.Equal(s.GetRightHalf(), res);
+        }
+
+        [Theory]
+        [InlineData("Hi, my name is tru-la-la", "la", "lu", "Hi, my name is tru-lu-lu")]
+        public void TestReplace(string s, string old, string @new, string res)
+        {
+            Assert.Equal(s.Replace(old, @new), res);
+        }
+
+        [Theory]
+        [InlineData("ABC", "\\u0041\\u0042\\u0043")]
+        public void TestCharsToCodes(string s, string res)
+        {
+            Assert.Equal(s.CharsToCodes(), res);
+        }
+
+        [Theory]
+        [InlineData("ABC", "CBA")]
+        [InlineData("What is it?", "?ti si tahW")]
+        public void TestGetReversed(string s, string res)
+        {
+            Assert.Equal(s.GetReversed(), res);
+        }
+
+        [Theory]
+        [InlineData("ABC", "abc")]
+        [InlineData("What is it?", "wHAT IS IT?")]
+        public void TestInverseCase(string s, string res)
+        {
+            Assert.Equal(s.InverseCase(), res);
+        }
+
+        [Theory]
+        [InlineData("ABCa", "BCDb")]
+        public void TestShiftInc(string s, string res)
+        {
+            Assert.Equal(s.ShiftInc(), res);
+        }
+
+        [Theory]
+        [InlineData("This is some text without comments and id", new long[0] {})]
+        [InlineData("This is another text without comments but with id: ABCD:1234",
+            new[] { 2882343476L })]
+        [InlineData("This is text with id: ABCD:1234 and comment: //bla bla AFFF:1010\ndgf",
+            new[] { 2882343476L })]
+        [InlineData("This is text with id: ABCD:1234 and comment: //bla bla AFFF:1010",
+            new[] { 2882343476L })]
+        [InlineData("Text with comment: /*b*/ and ids: FFFF:0000, ABCD:1234",
+            new[] { 4294901760L, 2882343476L })]
+        [InlineData("Text with comment: /*Comment with id\n FFFF:0000 bla bla*/ and ids: FFFF:0000, ABCD:1234", 
+            new[] { 4294901760L, 2882343476L })]
+        public void TestGetUsedObjects(string s, long[] res)
+        {
+            Assert.Equal(s.GetUsedObjects(), res.ToImmutableList());
+        }
     }
 }
--- course-2018-1/exercises/05-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub/RicksMercilessEncryptor.cs
+++ course-2018-1/exercises/05-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub/RicksMercilessEncryptor.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace WubbaLubbaDubDub
 {
@@ -12,7 +14,7 @@ namespace WubbaLubbaDubDub
         public static string[] SplitToLines(this string text)
         {
             // У строки есть специальный метод. Давай здесь без регулярок
-            throw new NotImplementedException();
+            return text.Split(new [] {Environment.NewLine}, StringSplitOptions.None);
         }
 
         /// <summary>
@@ -21,7 +23,7 @@ namespace WubbaLubbaDubDub
         public static string[] SplitToWords(this string line)
         {
             // А вот здесь поиграйся с регулярками.
-            throw new NotImplementedException();
+            return Regex.Split(line, "[^a-zA-Z]+");
         }
 
         /// <summary>
@@ -31,7 +33,7 @@ namespace WubbaLubbaDubDub
         public static string GetLeftHalf(this string s)
         {
             // у строки есть метод получения подстроки
-            throw new NotImplementedException();
+            return s.Substring(0, s.Length / 2);
         }
 
         /// <summary>
@@ -40,7 +42,7 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string GetRightHalf(this string s)
         {
-            throw new NotImplementedException();
+            return s.Substring(s.Length / 2);
         }
 
         /// <summary>
@@ -49,7 +51,7 @@ namespace WubbaLubbaDubDub
         public static string Replace(this string s, string old, string @new)
         {
             // и такой метод у строки, очевидно, тоже есть
-            throw new NotImplementedException();
+            return s.Replace(old, @new);
         }
 
         /// <summary>
@@ -65,7 +67,12 @@ namespace WubbaLubbaDubDub
                 FYI: локальную функцию можно объявлять даже после строки с return.
                 То же самое можно сделать и для всех оставшихся методов.
             */
-            throw new NotImplementedException();
+            string CharToCode(char ch)
+            {
+                return $"\\u{Convert.ToInt32(ch):X4}";
+            }
+
+            return string.Join("", s.Select(CharToCode));
         }
 
         /// <summary>
@@ -77,7 +84,7 @@ namespace WubbaLubbaDubDub
                 Собрать строку из последовательности строк можно несколькими способами.
                 Один из низ - статический метод Concat. Но ты можешь выбрать любой.
             */
-            throw new NotImplementedException();
+            return String.Concat(s.Reverse());
         }
 
         /// <summary>
@@ -90,7 +97,12 @@ namespace WubbaLubbaDubDub
                 На минуту задержись здесь и посмотри, какие еще есть статические методы у char.
                 Например, он содержит методы-предикаты для определения категории Юникода символа, что очень удобно.
             */
-            throw new NotImplementedException();
+            char InverseCharCase(char ch)
+            {
+                return char.IsUpper(ch) ? char.ToLower(ch) : char.ToUpper(ch);
+            }
+
+            return string.Concat(s.Select(InverseCharCase));
         }
 
         /// <summary>
@@ -99,7 +111,12 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string ShiftInc(this string s)
         {
-            throw new NotImplementedException();
+            string ShiftCharInc(char ch)
+            {
+                return char.ConvertFromUtf32(Convert.ToInt32(ch) + 1);
+            }
+
+            return string.Concat(s.Select(ShiftCharInc));
         }
 
 
@@ -117,7 +134,24 @@ namespace WubbaLubbaDubDub
                 Задача на поиграться с регулярками - вся сложность в том, чтобы аккуратно игнорировать комментарии.
                 Экспериментировать онлайн можно, например, здесь: http://regexstorm.net/tester и https://regexr.com/
             */
-            throw new NotImplementedException();
+            string[] noComments = Regex.Split(text, "/\\*(.|\n)*\\*/|//(.)*(\n|$)");
+
+            Regex idRegex = new Regex("[0-9A-Fa-f]{4}:[0-9A-Fa-f]{4}");
+
+            IEnumerable<string> SelectString(string s)
+            {
+                return idRegex.Matches(s).Select(m => m.Groups[0].Value);
+            }
+
+            long ConvertStringToLong(string s)
+            {
+                return Convert.ToInt64(s.Replace(":", ""), 16);
+            }
+
+            return noComments
+                .SelectMany(SelectString)
+                .Select(ConvertStringToLong)
+                .ToImmutableList();
         }
 
         #endregion
