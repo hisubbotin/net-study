484ac7ebce2f5f40358a0c1dba3b545730a9643a
anatoly kozlov <kozlof9@yandex.ru>
2018-10-29 13:02:17 +0300

    additional tests

--- course-2018-1/exercises/03-boring-vector/BoringVector/BoringVector.Tests/UnitTest1.cs
+++ course-2018-1/exercises/03-boring-vector/BoringVector/BoringVector.Tests/UnitTest1.cs
@@ -1,6 +1,5 @@
 using System;
 using Xunit;
-
 namespace BoringVector.Tests
 {
     public class UnitTest1
@@ -19,6 +18,24 @@ namespace BoringVector.Tests
         public void SquareLengthTest(double x, double y, double res)
         {
             Assert.Equal(res, new Vector(x, y).SquareLength());
+        }
+
+        [Theory]
+        [InlineData(1, -1)]
+        [InlineData(2, 2)]
+        [InlineData(2, -2)]
+        public void NormalizeLengthTest(double x, double y)
+        {
+            Assert.InRange<Double>( new Vector(x, y).Normalize().SquareLength(), 1.0 - 1e-6, 1.0 + 1e-6);
+        }
+
+        [Theory]
+        [InlineData(1, 0, 0, 1, VectorExtensions.Relation.Orthogonal)]
+        [InlineData(2, 2, 4, 4, VectorExtensions.Relation.Parallel)]
+        [InlineData(1, 1, 1, 2, VectorExtensions.Relation.General)]
+        internal void GetRelationTest(double x, double y, double x2, double y2, VectorExtensions.Relation relation)
+        {
+            Assert.Equal(relation, new Vector(x, y).GetRelation(new Vector(x2, y2)));
         }
 
         [Fact]
