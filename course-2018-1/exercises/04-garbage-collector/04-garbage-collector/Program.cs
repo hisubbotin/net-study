e5a71006a90e9ef9a6b9d69af951a58f30975068
unknown <evgeniy.eg1@yandex.ru>
2018-10-29 05:52:15 +0300

    tmp_changes

---
+++ course-2018-1/exercises/04-garbage-collector/04-garbage-collector/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace _04_garbage_collector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// https://habr.com/post/83929/ 
+            var decimalvar = new double[85000 / sizeof(decimal)];
+            Console.WriteLine("Gen: {0}", GC.GetGeneration(decimalvar));
+
+            //Generations();
+        }
+
+    
+
+        //private static void Generations()
+        //{
+        //    var sw = new Stopwatch();
+
+        //    for (var i = 0; i < 1000; ++i)
+        //    {
+        //        _ = new int[100500];
+        //    }
+
+        //    if (GC.CollectionCount(0) <= 0) return;
+        //    sw.Start();
+        //    GC.Collect(0, GCCollectionMode.Forced);
+        //    GC.WaitForPendingFinalizers();
+        //    sw.Stop();
+        //    Console.WriteLine("Generation: 0\tElapsed: {0}", sw.Elapsed);
+
+        //    if (GC.CollectionCount(1) <= 0) return;
+        //    sw.Restart();
+        //    GC.Collect(1, GCCollectionMode.Forced);
+        //    GC.WaitForPendingFinalizers();
+        //    sw.Stop();
+        //    Console.WriteLine("Generation: 1\tElapsed: {0}", sw.Elapsed);
+
+        //    if (GC.CollectionCount(2) <= 0) return;
+        //    sw.Restart();
+        //    GC.Collect(2, GCCollectionMode.Forced);
+        //    GC.WaitForPendingFinalizers();
+        //    sw.Stop();
+        //    Console.WriteLine("Generation: 2\tElapsed: {0}", sw.Elapsed);
+        //}
+    }
+}
---
+++ course-2018-1/exercises/04-garbage-collector/04-garbage-collector/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("04-garbage-collector")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("04-garbage-collector")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("e68dc093-d143-4116-867b-114c6b2f2e62")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
