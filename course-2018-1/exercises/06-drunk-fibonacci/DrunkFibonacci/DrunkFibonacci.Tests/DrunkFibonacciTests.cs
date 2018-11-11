ba1ac238fa6eab2f36ade4eac49907112dc92c93
wiki-fan <ph134@yandex.ru>
2018-11-11 17:28:12 +0300

    Empty tests

---
+++ course-2018-1/exercises/06-drunk-fibonacci/DrunkFibonacci/DrunkFibonacci.Tests/DrunkFibonacciTests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+using DrunkFibonacci;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DrunkFibonacci.Tests
+{
+	public class DrunkFibonacciTests
+	{
+		
+	}
+}
