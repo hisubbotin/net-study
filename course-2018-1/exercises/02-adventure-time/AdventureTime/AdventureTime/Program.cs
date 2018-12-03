14e12a7b807069da60f8b61c0cb3b4ec88441796
Maxim Kochukov <kochukov.ma@phystech.edu>
2018-12-03 05:40:17 +0400

    [02-adventure-time] Kochukov Maxim

--- course-2018-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ course-2018-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -1,9 +1,48 @@
-﻿namespace AdventureTime
+﻿using System;
+
+namespace AdventureTime
 {
     internal class Program
     {
         private static void Main()
         {
+            System.Console.WriteLine(AdventureTime.Time.WhatTimeIsIt());
+
+            System.Console.WriteLine(AdventureTime.Time.WhatTimeIsItInUtc());
+
+            System.Console.WriteLine(
+                AdventureTime.Time.SpecifyKind(System.DateTime.Parse("03.12.18 05:39:10"), System.DateTimeKind.Local));
+
+            string curRoundTime;
+            System.Console.WriteLine(curRoundTime =
+                AdventureTime.Time.ToRoundTripFormatString(System.DateTime.Now));
+
+            System.Console.WriteLine(AdventureTime.Time.ParseFromRoundTripFormat(curRoundTime));
+
+            System.Console.WriteLine(AdventureTime.Time.ToUtc(System.DateTime.Now));
+
+            System.Console.WriteLine(AdventureTime.Time.AddTenSeconds(System.DateTime.Now));
+
+            System.Console.WriteLine(AdventureTime.Time.GetHoursBetween(System.DateTime.Now, System.DateTime.Today));
+
+            System.Console.WriteLine(AdventureTime.Time.GetTotalMinutesInThreeMonths());
+
+            System.Console.WriteLine(AdventureTime.Time.GetAdventureTimeDurationInMinutes_ver0_Dumb());
+
+            System.Console.WriteLine(AdventureTime.Time.GetAdventureTimeDurationInMinutes_ver1_FeelsSmarter());
+
+            System.Console.WriteLine(AdventureTime.Time.GetAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+
+            System.Console.WriteLine(AdventureTime.Time.GetAdventureTimeDurationInMinutes_ver3_NodaTime());
+
+            System.Console.WriteLine(AdventureTime.Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver0_Dumb());
+
+            System.Console.WriteLine(AdventureTime.Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+
+            System.Console.WriteLine(AdventureTime.Time.AreEqualBirthdays(
+                System.DateTime.Parse("1911-12-03"),
+                System.DateTime.Parse("03.12.2011")));
         }
+    }
     }
 }
