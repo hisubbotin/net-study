b89725408fe711120c21379ebe8a54617be14380
Евгений Суханов < >
2019-01-28 00:04:29 +0300

    gc

--- exercises/gc_ex/gc_ex/Program.cs
+++ exercises/gc_ex/gc_ex/Program.cs
@@ -2,84 +2,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
+using System.Configuration;
 
-namespace GarbageCollector1
+namespace gc_exercise
 {
     class Program
     {
-        static void LargeObjectHeap()
+        static bool checkForNotify = false;
+        
+        static bool bAllocate = false;
+        
+        static bool finalExit = false;
+        static List<byte[]> load = new List<byte[]>();
+
+
+        public static void Main(string[] args)
         {
+            GC.RegisterForFullGCNotification(10, 10);
+            Console.WriteLine("Registered for GC notification.");
 
-            UInt64 size = 1;
-            var ints64 = new UInt64[size];
-            while (GC.GetGeneration(ints64) < 2)
-            {
-                size += 1;
-                ints64 = new UInt64[size];
-            }
-            Console.WriteLine("UInt64 : {0}", size * sizeof(UInt64));
+            checkForNotify = true;
+            bAllocate = true;
+                
+            new Thread(new ThreadStart(WaitForFullGCProc)).Start();
 
-            size = 1;
-            var ints32 = new UInt32[size];
-            while (GC.GetGeneration(ints32) < 2)
-            {
-                size += 1;
-                ints32 = new UInt32[size];
-            }
-            Console.WriteLine("UInt32 : {0}", size * sizeof(UInt32));
+                int lastCollCount = 0;
+                int newCollCount = 0;
 
-            size = 1;
-            var ints16 = new UInt16[size];
-            while (GC.GetGeneration(ints16) < 2)
-            {
-                size += 1;
-                ints16 = new UInt16[size];
-            }
-            Console.WriteLine("UInt16 : {0}", size * sizeof(UInt16));
-        }
-
-        static void CleanUpGen(int gen)
-        {
-            Stopwatch timer = new Stopwatch();
-
-            GC.TryStartNoGCRegion(10000 * 1024 * sizeof(int) * 2);
-
-            ArrayList lst = new ArrayList();
-            for (int i = 0; i < 10000; ++i)
-            {
-                lst.Add(new int[1024]);
-            }
-
-            int temp = gen - 1;
-
-            timer.Start();
-
-            GC.EndNoGCRegion();
-
-            while (temp >= 0) {
-                GC.Collect(temp, GCCollectionMode.Forced);
-                GC.WaitForPendingFinalizers();
-                temp--;
-            }
-            GC.Collect(gen, GCCollectionMode.Forced);
-            GC.WaitForPendingFinalizers();
-
-            timer.Stop();
-
-            Console.WriteLine("Generation {0}: {1}", gen, timer.Elapsed);
-            Console.WriteLine();
-        }
+                while (true)
+                {
+                    if (bAllocate)
+                    {
+                        load.Add(new byte[1000]);
+                        newCollCount = GC.CollectionCount(2);
+                        if (newCollCount != lastCollCount)
+                        {
+                            Console.WriteLine("Gen 2 collection count: {0}", GC.CollectionCount(2).ToString());
+                            lastCollCount = newCollCount;
+                        }
+                            
+                        if (newCollCount == 5)
+                        {
+                            finalExit = true;
+                            checkForNotify = false;
+                            break;
+                        }
+                    }
+                }
 
 
-        static void Main(string[] args)
-        {
-            LargeObjectHeap();
-
-            CleanUpGen(0);
-            CleanUpGen(1);
-            CleanUpGen(2);
+            finalExit = true;
+            checkForNotify = false;
+            GC.CancelFullGCNotification();
 
             Console.ReadLine();
         }
+
+        public static void WaitForFullGCProc()
+        {
+            while (true)
+            {
+                while (checkForNotify)
+                {
+                    Console.WriteLine("HERE");
+                    GCNotificationStatus s = GC.WaitForFullGCApproach();
+                    if (s == GCNotificationStatus.Succeeded)
+                    {
+                        Console.WriteLine("WaitForFullGCApproach");
+                        bAllocate = false;
+                        load.Clear();
+                        GC.Collect();
+                        Console.WriteLine("Induced a collection.");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("GC Notification cancelled.");
+                        break;
+                    }
+
+                    Stopwatch timer = new Stopwatch();
+                    timer.Start();
+                    GCNotificationStatus status = GC.WaitForFullGCComplete();
+                    if (status == GCNotificationStatus.Succeeded)
+                    {
+                        timer.Stop();
+                        Console.WriteLine("WaitForFullGCComplete: {0}", timer.Elapsed.ToString());
+                        bAllocate = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("GC Notification cancelled.");
+                        break;
+                    }
+                }
+
+                Thread.Sleep(500);
+                if (finalExit)
+                {
+                    break;
+                }
+            }
+
+        }
+
     }
 }
