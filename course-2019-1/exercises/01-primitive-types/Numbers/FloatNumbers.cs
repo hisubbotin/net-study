b057237ef6982a671593ebbe6939e85260e2c5c6
Skakovskiy Yuriy <geymer_98@mail.ru>
2019-04-04 08:38:52 +0300

    dsfdsfs

--- course-2019-1/exercises/01-primitive-types/Numbers/FloatNumbers.cs
+++ course-2019-1/exercises/01-primitive-types/Numbers/FloatNumbers.cs
@@ -13,11 +13,11 @@ namespace Numbers
         /// </summary>
         internal static double GetNaN()
         {
+            return 0.0 / 0.0;
             /*
                 Необходимо вернуть значение, не используя непосредственно саму константу.
                 Для этого подумай, какой смысл в себе несет эта константа и где бы она могла стать результатом операции или вычисления функции.
             */
-            throw new NotImplementedException();
         }
 
         /// <summary>
@@ -27,21 +27,31 @@ namespace Numbers
         /// <returns>True, если число имеет значение "не число", иначе false.</returns>
         internal static bool IsNaN(double d)
         {
+            return double.IsNaN(d);
             // Подсказка: по аналогии с константами типа int, у типа double тоже есть свой набор констант.
-            throw new NotImplementedException();
         }
-        
+
         /// <summary>
         /// Возвращает результат сравнения двух вещественнозначных чисел.
         /// </summary>
         /// <returns>-1 - первое меньше второго, 0 - значения равны, 1 - первое больше второго.</returns>
-        internal static int Compare(/* дополни сигнатуру метода как считаешь правильным */)
+        internal static int Compare(double first, double second, double eps)
         {
+            if (first < second - eps) {
+                return -1;
+            }
+
+            if (second < first - eps)
+            {
+                return 1;
+            }
+
+            return 0;
+
             /*
                 Подумай, почему это задание дано в части про вещественнозначные числа. И почему не дана полная сигнатура метода.
                 Если сходу идей нет, перестань искать подвох и просто реализуй дословно. Теперь еще раз посмотри на код и подумай в чем может быть проблема, сколько должно быть аргументов.
             */
-            throw new NotImplementedException();
         }
 
         // и все?!! О_о
--- course-2019-1/exercises/01-primitive-types/Numbers/Program.cs
+++ course-2019-1/exercises/01-primitive-types/Numbers/Program.cs
@@ -17,8 +17,13 @@ namespace Numbers
         private static void Main()
         {
             // твои бро: Console.WriteLine и Console.ReadLine
-
+            //Console.WriteLine(Integers.CubeWithOverflowCheck(1000));
+            Console.WriteLine(Integers.CubeWithoutOverflowCheck(Integers.HalfIntMaxValue()));
+            Console.WriteLine(Integers.HalfIntMaxValue());
             Console.WriteLine("Hello World!");
+            Console.WriteLine(Integers.Parse("5556"));
+            Console.WriteLine(Integers.ToHexString(256));
+            Console.WriteLine(FloatNumbers.Compare(3,3.3,0.5));
         }
     }
 }
