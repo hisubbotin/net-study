130125e0ae39a45ad7b39eb5f03177ee00010974
Peter <mokrov.pv@phystech.edu>
2019-04-11 20:20:19 +0300

    first task has been done

--- course-2019-1/exercises/01-primitive-types/Numbers/Program.cs
+++ course-2019-1/exercises/01-primitive-types/Numbers/Program.cs
@@ -14,11 +14,28 @@ namespace Numbers
 {
     internal class Program
     {
+        /* 
         private static void Main()
         {
             // твои бро: Console.WriteLine и Console.ReadLine
 
             Console.WriteLine("Hello World!");
+        }
+        */
+        public static void Main()
+        {
+            var r = new RefType { X = 1 };
+            RefMethod(r);
+            Console.WriteLine(r.X);
+            RefMethod(ref r);
+            Console.WriteLine(r.X);
+        }
+
+        class RefType { public Int32 X; }
+        static void RefMethod(RefType r) { r = new RefType { X = 15 }; }
+        static void RefMethod(ref RefType r)
+        {
+            r = new RefType { X = 15 };
         }
     }
 }
