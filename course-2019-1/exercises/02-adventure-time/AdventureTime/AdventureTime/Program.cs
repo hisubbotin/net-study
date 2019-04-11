059c2ad8594a675e70bebaa7d5d1732b46ac529e
mary3000 <mary.fifa3000@gmail.com>
2019-04-11 11:07:45 +0300

    tests & fixes

--- course-2019-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ course-2019-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -1,9 +1,39 @@
-﻿namespace AdventureTime
+﻿using System;
+
+namespace AdventureTime
 {
     internal class Program
     {
         private static void Main()
         {
+            var now = Time.WhatTimeIsIt();
+            
+            Console.WriteLine(now);
+            Console.WriteLine(Time.WhatTimeIsItInUtc());
+            var local = Time.SpecifyKind(now, DateTimeKind.Local);
+            Console.WriteLine(local);
+
+            var nowUtc = Time.SpecifyKind(now, DateTimeKind.Utc);
+            
+            Console.WriteLine(Time.ToRoundTripFormatString(now));
+            var rtf = Time.ToRoundTripFormatString(nowUtc);
+            Console.WriteLine(rtf);
+            Console.WriteLine(Time.ParseFromRoundTripFormat(rtf));
+            Console.WriteLine(Time.ToUtc(local));
+            Console.WriteLine(Time.AddTenSeconds(now));
+            Console.WriteLine(Time.AddTenSecondsV2(now));
+            Console.WriteLine(Time.GetHoursBetween(DateTime.Today, now));
+            var dt0 = new DateTime(2000, 1, 12);
+            var dt1 = new DateTime(2020, 1, 12);
+            Console.WriteLine(Time.AreEqualBirthdays(dt0, dt1));
+            
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver0_Dumb());
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver1_FeelsSmarter());
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver3_NodaTime());
+            Console.WriteLine(Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver0_Dumb());
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+            Console.WriteLine(Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+            
         }
     }
 }
--- course-2019-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Time.cs
+++ course-2019-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Time.cs
@@ -127,9 +127,7 @@ namespace AdventureTime
         public static int GetTotalMinutesInThreeMonths()
         {
             // ну тут все просто и очевидно, если сделал остальные и подумал над вопросами в комментах.
-            var dt1 = new DateTime(0, 3, 0);
-            var dt0 = new DateTime(0, 0, 0);
-            return (dt1 - dt0).Minutes;
+            throw new ArgumentException();
         }
 
         #region Adventure time saga
@@ -214,8 +212,8 @@ namespace AdventureTime
                 ниже ты найдешь готовый метод GetZonedTime. Просто посмотри на него (можешь даже посмотреть методы и свойства типа TimeZoneInfo, если интересно) и воспользуйся им для вычисления правильного времени
                 "отбытия" и "прибытия" наших героев. Затем посчитай длительность путешествия. Также даны правильные идентификаторы зон.
             */
-            const string moscowZoneId = "Russian Standard Time";
-            const string londonZoneId = "GMT Standard Time";
+            const string moscowZoneId = "Europe/Moscow";
+            const string londonZoneId = "Europe/London";
 
             var local = new DateTime(2010, 3, 28, 2, 15, 0);
 
@@ -230,8 +228,8 @@ namespace AdventureTime
             /*
                 Реши по аналогии с предыдущим методом и проверь, что оба метода действительно возвращают одно и то же время (и что оно правильное).
             */
-            const string moscowZoneId = "Russian Standard Time";
-            const string londonZoneId = "GMT Standard Time";
+            const string moscowZoneId = "Europe/Moscow";
+            const string londonZoneId = "Europe/London";
             
             var msk = new DateTime(2010, 3, 28, 3, 15, 0);
             var london = new DateTime(2010, 3, 28, 1, 15, 0);
@@ -295,7 +293,7 @@ namespace AdventureTime
         /// <returns>True - если родились в один день, иначе - false.</returns>
         internal static bool AreEqualBirthdays(DateTime person1Birthday, DateTime person2Birthday)
         {
-            return person1Birthday.Day == person2Birthday.Day;
+            return person1Birthday.Day == person2Birthday.Day && person1Birthday.Month == person2Birthday.Month;
         }
     }
 }
