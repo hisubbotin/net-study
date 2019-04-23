652741b071b18a3c7d041916296ecea26d4cb31f
Andrey Semenov <andrs271098@mail.ru>
2019-04-24 02:28:08 +0300

    ex-2

--- course-2019-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ course-2019-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -1,9 +1,42 @@
-п»їnamespace AdventureTime
+using System;
+
+namespace AdventureTime
 {
     internal class Program
     {
         private static void Main()
         {
+            DateTime now = Time.WhatTimeIsIt();
+
+            // протестим работу всех функций
+            // ToRoundTripFormatString
+            Console.WriteLine(now);
+            Console.WriteLine(Time.ToRoundTripFormatString(Time.SpecifyKind(now, DateTimeKind.Utc)));
+
+            // ParseFromRoundTripFormat
+            Console.WriteLine(Time.ParseFromRoundTripFormat(Time.ToRoundTripFormatString(Time.SpecifyKind(now, DateTimeKind.Local))));
+
+            // AddTenSeconds
+            Console.WriteLine(Time.AddTenSeconds(Time.ParseFromRoundTripFormat("27.10.1998")));
+            Console.WriteLine(Time.AddTenSecondsV2(Time.ParseFromRoundTripFormat("27.10.1998")));
+
+            // GetTotalMinutesInThreeMonths
+            //Console.WriteLine(Time.GetTotalMinutesInThreeMonths());
+
+            // GetHoursBetween
+            Console.WriteLine(Time.GetHoursBetween(Time.ParseFromRoundTripFormat("20.10.1998"), Time.ParseFromRoundTripFormat("27.10.1998")));
+
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver0_Dumb());
+            Console.WriteLine(Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver0_Dumb());
+
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver1_FeelsSmarter());
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver3_NodaTime());
+            Console.WriteLine(Time.GetAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+            Console.WriteLine(Time.GetGenderSwappedAdventureTimeDurationInMinutes_ver2_FeelsLikeRocketScience());
+
+            Console.WriteLine(Time.AreEqualBirthdays(Time.ParseFromRoundTripFormat("27.10.1998"), Time.ParseFromRoundTripFormat("20.10.1998")));
+            Console.WriteLine(Time.AreEqualBirthdays(Time.ParseFromRoundTripFormat("27.10.1998"), Time.ParseFromRoundTripFormat("27.10.2000")));
+            var x = Console.ReadLine();
         }
     }
 }
