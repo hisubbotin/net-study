822b483ee6934fcfaa33cdb3f87eeee227e47b3c
Alexey <petrovalex_99@mail.ru>
2019-04-25 10:58:09 +0300

    Checked

--- course-2019-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ course-2019-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -7,9 +7,14 @@ namespace AdventureTime
     {
         private static void Main()
         {
+
             DateTime a = Time.WhatTimeIsIt();
             Console.WriteLine(a.ToString());
-            a = Time.ParseFromRoundTripFormat(a.ToString());
+            DateTime q = Time.ParseFromRoundTripFormat(Time.ToRoundTripFormatString(a));
+            if (a==q)
+            {
+                Console.WriteLine("True");
+            }
             Console.WriteLine(a);
             Console.WriteLine(Time.ParseFromRoundTripFormat("01.02.1999"));
             Console.WriteLine(Time.AreEqualBirthdays(a, Time.ParseFromRoundTripFormat("01.02.1999")));
