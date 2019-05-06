964110d801025c95f6b2186dc352650f9727b53f
Vitaly Ivanin <alloky2011@gmail.com>
2019-05-06 23:57:14 +0300

    ex 4 added

---
+++ course-2019-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/StringBench/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace StringBench
+{
+    [InProcessAttribute] // использовано из-за антивирусов
+    [RPlotExporter, RankColumn]
+     public class StringJoinTest
+    {
+        public List<string> data;
+
+
+        [Params(4, 1000, 10000, 1000000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            data = new List<string>();
+            for(int i = 0; i < N; ++i)
+            {
+                data.Add("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789");
+            }
+        }
+
+
+        [Benchmark]
+        public string Join_tets() => String.Join("", data);
+
+        [Benchmark]
+        public string SB_test()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var row in data)
+            {
+                if (row.Length > 0)
+                {
+                    sb.Append(row);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<StringJoinTest>();
+        }
+    }
+}
--- course-2019-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
+++ course-2019-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
@@ -1,10 +1,91 @@
 using System;
+using System.Collections.Immutable;
 using Xunit;
 
 namespace WubbaLubbaDubDub.Tests
 {
     public class RicksMercilessEncryptorTests
     {
-        
+        [Theory]
+        [InlineData("new test string", new[] { "new test string" })]
+        [InlineData("just\ntesting\n\n\nthis\nmethod", new[] { "just", "testing", "" ,"", "this", "method" })]
+        public void SplitToLines(string before, string[] expected)
+        {
+            Assert.Equal(expected, before.SplitToLines());
+        }
+
+        [Theory]
+        [InlineData("new test string", new[] { "new", "test", "string" })]
+        [InlineData("just\ntesting, another\n\n\nthis\nmethod.", new[] { "just", "testing", "another", "this", "method" })]
+        public void SplitToWords(string before, string[] expected)
+        {
+            //Assert.Equal(expected, before.SplitToLines());
+        }
+
+        [Theory]
+        [InlineData("abcdef", "abc")]
+        [InlineData("abcdefg", "abc")]
+        [InlineData("", "")]
+        public void GetLeftHalf(string before, string expected)
+        {
+            Assert.Equal(expected, before.GetLeftHalf());
+        }
+
+        [Theory]
+        [InlineData("abcdef", "def")]
+        [InlineData("abcdefg", "defg")]
+        [InlineData("", "")]
+        public void GetRightHalf(string before, string expected)
+        {
+            Assert.Equal(expected, before.GetRightHalf());
+        }
+
+        [Theory]
+        [InlineData(new[] { "new test string is new", "new", "old"},  "old test string is old" )]
+        public void Replace(string[] before, string expected)
+        {
+            Assert.Equal(expected, before[0].Replace(before[1], before[2]));
+        }
+
+        [Theory]
+        [InlineData("alloky1234", @"\u0061\u006C\u006C\u006F\u006B\u0079\u0031\u0032\u0033\u0034")]
+        public void CharsToCodes(string before, string expected)
+        {
+            var test = before.CharsToCodes();
+            Assert.Equal(expected, before.CharsToCodes());
+        }
+
+        [Theory]
+        [InlineData("abcdef", "fedcba")]
+        [InlineData("abcdefg", "gfedcba")]
+        [InlineData("", "")]
+        public void Reversed(string before, string expected)
+        {
+            Assert.Equal(expected, before.GetReversed());
+        }
+
+        [Theory]
+        [InlineData("aBc1$", "AbC1$")]
+        [InlineData("abc D,ef?", "ABC d,EF?")]
+        [InlineData("", "")]
+        public void InverseCase(string before, string expected)
+        {
+            Assert.Equal(expected, before.InverseCase());
+        }
+
+        [Theory]
+        [InlineData("abc", "bcd")]
+        public void ShiftInc(string before, string expected)
+        {
+            Assert.Equal(expected, before.ShiftInc());
+        }
+
+
+        [Theory]
+        [InlineData("¶0:2¶//¶100:200¶\n/** ¶0:13¶ **/ ¶0:12¶ ¶0:20¶", new[] { 2L, 18L, 32L })]
+        public void GetUsedObjects(string before, long[] expected)
+        {
+            Assert.Equal(expected, before.GetUsedObjects());
+        }
     }
 }
