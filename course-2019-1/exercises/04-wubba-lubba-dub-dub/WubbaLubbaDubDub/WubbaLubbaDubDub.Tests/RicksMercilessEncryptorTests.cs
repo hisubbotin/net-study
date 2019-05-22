a2d98c30de10ee5e7f1c0cfd92eb6cd410c8feef
alfekka <47663948+alfekka@users.noreply.github.com>
2019-05-23 02:36:04 +0300

    Add files via upload

---
+++ course-2019-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
@@ -0,0 +1,50 @@
+using System;
+using Xunit;
+using System.Collections.Immutable;
+
+namespace WubbaLubbaDubDub.Tests
+{
+    public class RicksMercilessEncryptorTests
+    {
+        [Fact]
+        public void Test()
+        {
+            string line = "izvinite ya probila dealines";
+            string[] p = line.SplitToWords();
+            Assert.Equal(p, line.SplitToWords());
+        }
+
+        [Fact]
+        public void TestSplitToLines()
+        {
+            string text = "this is\nlupa\nthis is\npupa";
+            string[] result = new[] { "this is", "lupa", "this is", "pupa"};
+            Assert.Equal(text.SplitToLines(), result);
+        }
+
+        [Fact]
+        public void TestHalf()
+        {
+            string line = "cheburek";
+            string p = line.GetLeftHalf();
+            Assert.Equal(p, line.GetRightHalf());
+        }
+
+        [Fact]
+        public void TestReplace()
+        {
+            string line = "lupa poluchil za sebya";
+            string p = line.Replace("sebya","pupu");
+            Assert.Equal(p, "lupa poluchil za pupu");
+        }
+
+        [Fact]
+        public void TestInverseCase()
+        {
+            string line = "HuBaHuBa";
+            string p = line.InverseCase();
+            Assert.Equal(p, "hUbAhUbA");
+        }
+        
+    }
+}
