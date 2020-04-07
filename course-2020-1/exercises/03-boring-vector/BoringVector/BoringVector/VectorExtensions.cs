13fa4905daf3c9804c758f154a1eeace26599e0b
Boris Zhestiankin <zhestyankin.ba@phystech.edu>
2020-04-07 19:02:37 +0300

    fix extensions accuracy

--- course-2020-1/exercises/03-boring-vector/BoringVector/BoringVector/VectorExtensions.cs
+++ course-2020-1/exercises/03-boring-vector/BoringVector/BoringVector/VectorExtensions.cs
@@ -2,14 +2,6 @@
 
 namespace BoringVector
 {
-    /*
-        Здесь тебе нужно написать класс с методами-расширениями структуры Vector:
-            - IsZero: проверяет, является ли вектор нулевым, т.е. его координаты близки к нулю (в эпсилон окрестности). За эпсилон здесь и далее берем 1e-6.
-            - Normalize: нормализует вектор
-            - GetAngleBetween: возвращает угол между двумя векторами в радианах. Примечание: нулевой вектор сонаправлен любому другому.
-            - GetRelation: возвращает значение перечесления VectorRelation(General, Parallel, Orthogonal) - отношение между двумя векторами("общий случай", параллельны, перпендикулярны). Перечисление задавать тоже тебе)
-    */
-    
     /// <summary>
     /// Отношение между двумя векторами (общий случай, параллельны, перпендикулярны)
     /// </summary>
@@ -30,6 +22,7 @@ namespace BoringVector
             const double eps = 1e-6;
             return Math.Sqrt(v.SquareLength()) < eps;
         }
+
         /// <summary>
         /// Возвращает <see cref="Vector"/>, отнормированный на его длину
         /// </summary>
@@ -37,9 +30,14 @@ namespace BoringVector
         /// <returns><see cref="Vector"/>, отнормированный на его длину</returns>
         public static Vector Normalize(this Vector v)
         {
-            var inverted_len = 1 / Math.Sqrt(v.SquareLength());
-            return v.Scale(inverted_len);
+            if (v.IsZero())
+            {
+                return v;
+            }
+
+            return v / Math.Sqrt(v.SquareLength());
         }
+
         /// <summary>
         /// Возвращает <see cref="double"/>, угол между двумя векторами в радианах
         /// </summary>
@@ -48,9 +46,25 @@ namespace BoringVector
         /// <returns><see cref="double"/>, угол между двумя векторами в радианах</returns>
         public static double GetAngleBetween(this Vector v, Vector u)
         {
-            var lengths_product = Math.Sqrt(v.SquareLength() * u.SquareLength());
-            return Math.Acos(v.DotProduct(u) / lengths_product);
+            if (v.IsZero() || u.IsZero())
+            {
+                return 0;
+            }
+
+            var cos = v.DotProduct(u) / Math.Sqrt(v.SquareLength() * u.SquareLength());
+            if (cos < -1)
+            {
+                return Math.PI;
+            }
+
+            if (cos > 1)
+            {
+                return 0;
+            }
+
+            return Math.Acos(cos);
         }
+
         /// <summary>
         /// Возвращает отношение (VectorRelation) между двумя векторами 
         /// </summary>
@@ -60,14 +74,13 @@ namespace BoringVector
         public static VectorRelation GetRelation(this Vector v, Vector u)
         {
             const double eps = 1e-6;
-            var cross_product = v.CrossProduct(u);
-            if (-eps < cross_product && cross_product < eps)
+            var angle = v.GetAngleBetween(u);
+            if (angle < eps || Math.PI - angle < eps)
             {
                 return VectorRelation.Parallel;
             }
             
-            var dot_product = v.DotProduct(u);
-            if (-eps < dot_product && dot_product < eps)
+            if (Math.Abs(angle - Math.PI / 2) < eps)
             {
                 return VectorRelation.Orthogonal;
             }
