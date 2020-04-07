bcce1550fdf7a04b03f925658e58f9c9a86e4a17
Boris Zhestiankin <zhestyankin.ba@phystech.edu>
2020-04-07 18:53:25 +0300

    fix mutability

--- course-2020-1/exercises/03-boring-vector/BoringVector/BoringVector/Vector.cs
+++ course-2020-1/exercises/03-boring-vector/BoringVector/BoringVector/Vector.cs
@@ -8,10 +8,10 @@ namespace BoringVector
     /// <summary>
     /// Класс для работы с векторами на плоскости
     /// </summary>
-    internal struct Vector
+    internal readonly struct Vector
     {
-        public double X;
-        public double Y;
+        public readonly double X;
+        public readonly double Y;
 
         /// <summary>
         /// Конструирует вектор из двух координат
@@ -32,28 +32,27 @@ namespace BoringVector
         {
             return X * X + Y * Y;
         }
+        
         /// <summary>
-        /// Складывает заданный <see cref="Vector"/> с текущим и возвращает сумму, <see cref="Vector"/>
+        /// Возвращает сумму текущего и заданного <see cref="Vector"/>
         /// </summary>
         /// <param name="v">Прибавляемый <see cref="Vector"/></param>
         /// <returns><see cref="Vector"/>, сумма векторов</returns>
         public Vector Add(Vector v)
         {
-            X += v.X;
-            Y += v.Y;
-            return this;
+            return new Vector(X + v.X, Y + v.Y);
         }
+        
         /// <summary>
-        /// Возвращает текущий <see cref="Vector"/>, умноженный на число <see cref="double"/>
+        /// Возвращает <see cref="Vector"/>, умноженный на число <see cref="double"/>
         /// </summary>
         /// <param name="k"><see cref="double"/>, коэффициент умножения</param>
         /// <returns><see cref="Vector"/>, умноженный на число <see cref="double"/></returns>
         public Vector Scale(double k)
         {
-            X *= k;
-            Y *= k;
-            return this;
+            return new Vector(k * X,  k * Y);
         }
+        
         /// <summary>
         /// Возвращает <see cref="double"/>, скалярное произведение с заданным <see cref="Vector"/> 
         /// </summary>
@@ -63,6 +62,7 @@ namespace BoringVector
         {
             return X * v.X + Y * v.Y;
         }
+        
         /// <summary>
         /// Возвращает <see cref="double"/>, модуль векторного произведения с заданным <see cref="Vector"/> 
         /// </summary>
@@ -72,6 +72,7 @@ namespace BoringVector
         {
             return Math.Abs(X * v.Y - Y * v.X);
         }
+        
         /// <summary>
         /// Возвращает <see cref="string"/>, строковое представление <see cref="Vector"/>
         /// </summary>
@@ -80,6 +81,7 @@ namespace BoringVector
         {
             return string.Concat("(", X.ToString(), "; )", Y.ToString(), ")");
         }
+        
         /// <summary>
         /// Складывает два объекта <see cref="Vector"/> и возвращает новый <see cref="Vector"/>
         /// </summary>
@@ -88,8 +90,9 @@ namespace BoringVector
         /// <returns><see cref="Vector"/>, сумма векторов</returns>
         public static Vector operator +(Vector v, Vector u)
         {
-            return u.Add(v);
+            return v.Add(u);
         }
+        
         /// <summary>
         /// Вычитает один <see cref="Vector"/> из другого и возвращает новый <see cref="Vector"/>
         /// </summary>
@@ -100,6 +103,7 @@ namespace BoringVector
         {
             return new Vector(v.X - u.X, v.Y - u.Y);
         }
+        
         /// <summary>
         /// Возвращает <see cref="Vector"/>, умноженный на число <see cref="double"/>
         /// </summary>
@@ -110,6 +114,7 @@ namespace BoringVector
         {
             return v.Scale(k);
         }
+        
         /// <summary>
         /// Возвращает <see cref="Vector"/>, умноженный на число <see cref="double"/>
         /// </summary>
@@ -120,6 +125,7 @@ namespace BoringVector
         {
             return v.Scale(k);
         }
+        
         /// <summary>
         /// Возвращает <see cref="Vector"/>, нормированный на число <see cref="double"/>
         /// </summary>
@@ -130,47 +136,25 @@ namespace BoringVector
         {
             return v.Scale(1 / k);
         }
+        
         /// <summary>
-        /// Позволяет использовать унарный + с <see cref="Vector"/>, не меняет его
+        /// Возвращает неизмененный <see cref="Vector"/>
         /// </summary>
-        /// <param name="v">Объект <see cref="Vector"/></param>
+        /// <param name="v">Исходный <see cref="Vector"/></param>
         /// <returns>Неизмененный <see cref="Vector"/></returns>
         public static Vector operator +(Vector v)
         {
             return v;
         }
+        
         /// <summary>
         /// Возвращает <see cref="Vector"/>, направленный противоположно данному
         /// </summary>
-        /// <param name="v">Объект <see cref="Vector"/></param>
+        /// <param name="v">Исходный <see cref="Vector"/></param>
         /// <returns><see cref="Vector"/>, направленный противоположно данному</returns>
         public static Vector operator -(Vector v)
         {
             return new Vector(-v.X, -v.Y);
         }
     }
-
-    #region 4. Тесты
-
-    /*
-            SquareLength
-            Add
-            Scale
-            DotProduct
-            CrossProduct
-
-        Примечание: структура Vector описана как internal структура, поэтому по умолчанию сборке BoringVector.Tests она не видна.
-        Чтобы она была видна, существует специальная директива компилятору:
-            [assembly: InternalsVisibleTo("XXX")]
-        , где XXX - название проекта, которому ты хочешь сделать видимыми свои internal'ы.
-
-        Можешь посмотреть, в задании [01-primitive-types] эта директива есть в файле Program.cs проекта Numbers.
-    */
-
-    #endregion
-
-
-    /*
-        На этом все. Время делать пулл реквест и наслаждаться заслуженным отдыхом :)
-    */
 }
