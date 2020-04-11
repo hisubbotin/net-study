51a73b63f27eea26ddc01cbd59ddee92a16476d6
Alena <grachevaalena39gkl@gmail.com>
2020-04-11 21:02:05 +0300

    number 4 done

---
+++ course-2020-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/Speed/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBenchmarks
+{
+    public class SpeedComparator
+    {
+        /// <summary>
+        /// Возвращает список из символов алфавита 
+        /// </summary>
+        private static List<string> GetAlphabet(bool upper)
+        {
+            List<string> alphabet = new List<string>();
+            int charValue = upper ? 65 : 97;
+            for (int ctr = 0; ctr <= 25; ctr++)
+                alphabet.Add(Convert.ToChar(charValue + ctr).ToString());
+            return alphabet;
+        }
+        // это будет наш тестовый ввод
+        List<string> test_str_list = GetAlphabet(true);
+        [Benchmark]
+        public string TestJoin()
+        {
+            return string.Join(" ", test_str_list);
+        }
+
+
+        [Benchmark]
+        public string TestStringBuilder()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(test_str_list);
+            return sb.ToString();
+        }
+
+        [Benchmark]
+        public string TestConcat()
+        {
+            string result = string.Empty;
+            foreach (string symbol in test_str_list)
+            {
+                result = String.Concat(result, symbol);
+            }
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SpeedComparator>();
+        }
+    }
+}
--- course-2020-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub/RicksMercilessEncryptor.cs
+++ course-2020-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub/RicksMercilessEncryptor.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace WubbaLubbaDubDub
 {
@@ -11,8 +13,9 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string[] SplitToLines(this string text)
         {
+            string[] strings = text.Split(new char[] { '\n' });
             // У строки есть специальный метод. Давай здесь без регулярок
-            throw new NotImplementedException();
+            return strings;
         }
 
         /// <summary>
@@ -20,8 +23,8 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string[] SplitToWords(this string line)
         {
-            // А вот здесь поиграйся с регулярками.
-            throw new NotImplementedException();
+            string[] strings = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return strings;
         }
 
         /// <summary>
@@ -31,7 +34,7 @@ namespace WubbaLubbaDubDub
         public static string GetLeftHalf(this string s)
         {
             // у строки есть метод получения подстроки
-            throw new NotImplementedException();
+            return  s.Substring(0, s.Length/2);
         }
 
         /// <summary>
@@ -40,7 +43,7 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string GetRightHalf(this string s)
         {
-            throw new NotImplementedException();
+            return s.Substring(s.Length / 2);
         }
 
         /// <summary>
@@ -48,8 +51,7 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string Replace(this string s, string old, string @new)
         {
-            // и такой метод у строки, очевидно, тоже есть
-            throw new NotImplementedException();
+            return s.Replace(old, @new);
         }
 
         /// <summary>
@@ -65,7 +67,18 @@ namespace WubbaLubbaDubDub
                 FYI: локальную функцию можно объявлять даже после строки с return.
                 То же самое можно сделать и для всех оставшихся методов.
             */
-            throw new NotImplementedException();
+            string CharToCode(char c)
+            {
+                return String.Format("\\u{0:x4}", (int)c);
+            }
+
+            string[] parts = new string[s.Length];
+            for (var i = 0; i < s.Length; ++i)
+            {
+                parts[i]=CharToCode(s[i]);
+            }
+
+            return string.Concat(parts);
         }
 
         /// <summary>
@@ -73,11 +86,18 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string GetReversed(this string s)
         {
+            char[] chars = new char[s.Length];
+            for (var i = s.Length - 1; i >= 0; --i)
+            {
+                chars[s.Length - i - 1] = s[i];
+            }
+            return string.Concat(chars);
+
             /*
                 Собрать строку из последовательности строк можно несколькими способами.
                 Один из низ - статический метод Concat. Но ты можешь выбрать любой.
             */
-            throw new NotImplementedException();
+
         }
 
         /// <summary>
@@ -85,12 +105,19 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string InverseCase(this string s)
         {
+
+            char[] chars = new char[s.Length];
+            for (var i = 0; i < s.Length; ++i)
+            {
+                chars[i] = (char.IsLower(s[i]) ? char.ToUpper(s[i]) : char.ToLower(s[i]));
+            }
+            return string.Concat(chars);
+            
             /*
                 Здесь тебе помогут статические методы типа char.
                 На минуту задержись здесь и посмотри, какие еще есть статические методы у char.
                 Например, он содержит методы-предикаты для определения категории Юникода символа, что очень удобно.
             */
-            throw new NotImplementedException();
         }
 
         /// <summary>
@@ -99,7 +126,13 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string ShiftInc(this string s)
         {
-            throw new NotImplementedException();
+            char[] chars = new char[s.Length];
+            for (var i = 0; i < s.Length; ++i)
+            {
+                chars[i] = (char)(s[i] + 1);
+            }
+            return string.Concat(chars);
+           
         }
 
 
@@ -107,9 +140,12 @@ namespace WubbaLubbaDubDub
 
         /// <summary>
         /// Возвращает список уникальных идентификаторов объектов, используемых в тексте <see cref="text"/>.
-        /// Идентификаторы объектов имеют длину 8байт и представлены в тексте в виде ¶X:Y¶, где X - старшие 4 байта, а Y - младшие 4 байта.
-        /// Текст <see cref="text"/> так же содержит строчные (//) и блоковые (/**/) комментарии, которые нужно игнорировать.
-        /// Т.е. в комментариях идентификаторы объектов искать не нужно. И, кстати, блоковые комментарии могут быть многострочными.
+        /// Идентификаторы объектов имеют длину 8байт и представлены в тексте в виде ¶X:Y¶, где X - старшие 4 байта, 
+        /// а Y - младшие 4 байта.
+        /// Текст <see cref="text"/> так же содержит строчные (//) и блоковые (/**/) комментарии,
+        /// которые нужно игнорировать.
+        /// Т.е. в комментариях идентификаторы объектов искать не нужно. И, кстати,
+        /// блоковые комментарии могут быть многострочными.
         /// </summary>
         public static IImmutableList<long> GetUsedObjects(this string text)
         {
@@ -117,8 +153,12 @@ namespace WubbaLubbaDubDub
                 Задача на поиграться с регулярками - вся сложность в том, чтобы аккуратно игнорировать комментарии.
                 Экспериментировать онлайн можно, например, здесь: http://regexstorm.net/tester и https://regexr.com/
             */
-            throw new NotImplementedException();
+            string withoutComments = Regex.Replace(text, @"(\/\*((?!\*\/)(.|\n))*\*\/)|(\/\/.*)", " ");
+            var result = Regex.Matches(withoutComments, @"([0-9A-F]{4}):([0-9A-F]{4})");
+            return result.OfType<Match>().Select(str => str.Value.Replace(":", "")).Distinct().Select(Id => Convert.ToInt64(Id)).ToImmutableList();
+           
         }
+        
 
         #endregion
     }
