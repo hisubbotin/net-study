23a3797367d95744c4d79c822e39648871a48076
Arman Stepanyan <stepanyan982@gmail.com>
2020-05-10 18:45:25 +0200

    Add task 3

---
+++ course-2020-1/exercises/03-boring-vector/BoringVector/BoringVector.Tests/VectorTests.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using Xunit;
+using BoringVector;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace BoringVector.Tests
+{
+    public class VectorTest
+    {
+        [Theory]
+        [InlineData(4, 4, 32)]
+        [InlineData(0, 0, 0)]
+        [InlineData(-3, 1, 10)]
+        public void Test_SquaredLength(double x, double y, double sqlen)
+        {
+            var v = new Vector(x, y);
+            Assert.Equal(v.SquareLength(), sqlen);
+        }
+
+        [Theory]
+        [InlineData(-1, 1, 1, -1, 0, 0)]
+        public void Test_Add(double vx, double vy, double ux, double uy, double ax, double ay)
+        {
+            var v = new Vector(vx, vy);
+            var u = new Vector(ux, uy);
+            var sum = v.Add(u);
+            Assert.Equal(sum.X, ax);
+            Assert.Equal(sum.Y, ay);
+
+        }
+
+        [Theory]
+        [InlineData(1, 1, 2, 2, 2)]
+        public void Test_Scale(double x, double y, double k, double ax, double ay)
+        {
+            var v = new Vector(x, y);
+            var scaled_v = v.Scale(k);
+            Assert.Equal(scaled_v.X, ax);
+            Assert.Equal(scaled_v.Y, ay);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 1, 1, 2)]
+        public void Test_DotProd(double vx, double vy, double ux, double uy, double prod)
+        {
+            var v = new Vector(vx, vy);
+            var u = new Vector(ux, uy);
+            var ans = v.DotProduct(u);
+            Assert.Equal(ans, prod);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 1, 1, 0)]
+        public void Test_CrossProd(double vx, double vy, double ux, double uy, double prod)
+        {
+            var v = new Vector(vx, vy);
+            var u = new Vector(ux, uy);
+            var ans = v.CrossProduct(u);
+            Assert.Equal(ans, prod);
+        }
+    }
+} 
--- course-2020-1/exercises/03-boring-vector/BoringVector/BoringVector/VectorExtensions.cs
+++ course-2020-1/exercises/03-boring-vector/BoringVector/BoringVector/VectorExtensions.cs
@@ -1,10 +1,56 @@
-﻿namespace BoringVector
+﻿using System;
+
+namespace BoringVector
+
 {
-    /*
-        Здесь тебе нужно написать класс с методами-расширениями структуры Vector:
-            - IsZero: проверяет, является ли вектор нулевым, т.е. его координаты близки к нулю (в эпсилон окрестности). За эпсилон здесь и далее берем 1e-6.
-            - Normalize: нормализует вектор
-            - GetAngleBetween: возвращает угол между двумя векторами в радианах. Примечание: нулевой вектор сонаправлен любому другому.
-            - GetRelation: возвращает значение перечесления VectorRelation(General, Parallel, Orthogonal) - отношение между двумя векторами("общий случай", параллельны, перпендикулярны). Перечисление задавать тоже тебе)
-    */
+    internal static class VectorExtensions
+    {
+        private const double EPS = 1e-6;
+
+        public enum VectorRelation : byte
+        {
+            General,
+            Parallel,
+            Orthogonal
+        }
+
+        public static bool IsZero(Vector v)
+        {
+            return v.SquareLength() < EPS * EPS;
+        }
+
+        public static Vector Normalize(Vector v)
+        {
+            double length = Math.Sqrt(v.SquareLength());
+            return v / length;
+        }
+
+        public static double GetAngleBetween(Vector v, Vector u)
+        {
+            if (IsZero(v) || IsZero(u))
+            {
+                return 0.0;
+            }
+
+            double vLength = Math.Sqrt(v.SquareLength()); 
+            double uLength = Math.Sqrt(u.SquareLength());
+
+            return Math.Acos(v.DotProduct(u) / vLength / uLength);
+        }
+
+        public static VectorRelation GetRelation(Vector v, Vector u)
+        {
+            if (v.DotProduct(u) < EPS)
+            {
+                return VectorRelation.Orthogonal;
+            }
+
+            if (v.CrossProduct(u) < EPS)
+            {
+                return VectorRelation.Parallel;
+            }
+
+            return VectorRelation.General;
+        }
+    }
 }
