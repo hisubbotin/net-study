6c5016f912c3fe9b6d85ab36c29c3bb9b6674d06
yaroslaw.serow <yaroslaw.serow@yandex.ru>
2020-05-13 18:29:10 +0300

    exersice 4

--- course-2020-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
+++ course-2020-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
@@ -1,10 +1,113 @@
-using System;
+﻿using System;
 using Xunit;
+using System.Linq;
 
 namespace WubbaLubbaDubDub.Tests
 {
     public class RicksMercilessEncryptorTests
     {
-        
+
+        [Fact]
+        public void SplitToLines_test()
+        {
+            string[] substrs = { "uta", "kek", "opa ! 1", " 123 " };
+
+            string s = String.Join("\n", substrs);
+            string[] result = s.SplitToLines();
+
+            Assert.Equal(result.Length, substrs.Length);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.Equal(substrs[i], result[i]);
+            }
+        }
+
+        [Fact]
+        public void SplitToWords_test()
+        {
+            string[] substrs = { "uta", "kek", "opa1", " 123 ", "uta", ";;;!!NOO", " " };
+            string[] realResult = { "uta", "kek", "opa", "uta", "NOO" };
+
+            string s = String.Join(" ", substrs);
+
+            string[] result = s.SplitToWords();
+
+            Assert.Equal(result, realResult);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("12345", "12")]
+        [InlineData("1234", "12")]
+        [InlineData("1", "")]
+        public void GetLeftHalf_test(string arg, string result)
+        {
+            Assert.Equal(result, arg.GetLeftHalf());
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("12345", "345")]
+        [InlineData("1234", "34")]
+        [InlineData("1", "1")]
+        public void GetRightHalf_test(string arg, string result)
+        {
+            Assert.Equal(result, arg.GetRightHalf());
+        }
+
+        [Theory]
+        [InlineData("", "u", "u", "")]
+        [InlineData("12345", "u", "u", "12345")]
+        [InlineData("12345", "23", "32", "13245")]
+        [InlineData("1111", "1", "11", "11111111")]
+        [InlineData("1223", "2", "3", "1333")]
+        [InlineData("12345", "12345", "", "")]
+        public void Replace_test(string arg, string old, string @new, string result)
+        {
+            Assert.Equal(result, arg.Replace(old, @new));
+        }
+
+        [Theory]
+        [InlineData("139", @"\u0031\u0033\u0039")]
+        public void CharsToCodes_test(string arg, string result)
+        {
+            Assert.Equal(result, arg.CharsToCodes());
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("1", "1")]
+        [InlineData("abc", "cba")]
+        [InlineData("ab ab ab", "ba ba ba")]
+        public void GetReversed_test(string arg, string result)
+        {
+            Assert.Equal(arg.GetReversed(), result);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("1", "1")]
+        [InlineData("abc ABC 123 Àáâ", "ABC abc 123 àÁÂ")]
+        public void InverseCase_test(string arg, string result)
+        {
+            Assert.Equal(result, arg.InverseCase());
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("1", "2")]
+        [InlineData("abcABC123Àáâ", "bcdBCD234Áâã")]
+        public void ShiftInc_test(string arg, string result)
+        {
+            Assert.Equal(result, arg.ShiftInc());
+        }
+
+        [Theory]
+        [InlineData("¶1234:0000¶ // ¶0000:0001¶ /* ¶0000:0002¶ \n ¶0000:0003¶ // ¶0000:0004¶ */ ¶0000:0005¶", new[] { 12340000L, 3L })]
+        [InlineData("¶0000:0000¶ /* ¶0000:0001¶ \n ¶0000:0002¶ // ¶0000:0003¶ */ ¶0000:1234¶", new[] { 0L, 1234L })]
+        private void TestGetUsedObjects(string text, long[] ids)
+        {
+            Assert.Equal(ids, text.GetUsedObjects().ToArray());
+        }
     }
 }
---
+++ course-2020-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+using System;
+using WubbaLubbaDubDub;
+using System.Text;
+
+namespace Program
+{
+    class Program
+    {
+        static string[] testStrings = "Все разработчики рано или поздно сталкиваются с регулярными выражениями.".Split();
+
+        static void ProccessJoin()
+        {
+            String.Join(" ", testStrings);
+        }
+
+        static void ProcessStringBuilder()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (string lexem in testStrings)
+            {
+                stringBuilder.Append(lexem);
+            }
+        }
+
+        static void ProcessConcat()
+        {
+            String.Concat(testStrings);
+        }
+
+        static void Main(string[] args)
+        {
+            int testSize = 10000000;
+
+            Stopwatch stopwatch = new Stopwatch();
+            TimeSpan ts = new TimeSpan();
+
+            stopwatch.Start();
+            for (int i = 0; i < testSize; ++i)
+            {
+                ProccessJoin();
+            }
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            Console.WriteLine("ProccessJoin(), size - " + testSize.ToString());
+            Console.WriteLine("total: " + ts.ToString());
+            Console.Write("ProccessJoin(), mean - ");
+            Console.WriteLine((ts.TotalMilliseconds / testSize).ToString());
+            Console.WriteLine();
+
+            stopwatch.Restart();
+            for (int i = 0; i < testSize; ++i)
+            {
+                ProcessStringBuilder();
+            }
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            Console.WriteLine("ProcessStringBuilder(), size - " + testSize.ToString());
+            Console.WriteLine("total: " + ts.ToString());
+            Console.Write("ProcessStringBuilder(), mean - ");
+            Console.WriteLine((ts.TotalMilliseconds / testSize).ToString());
+            Console.WriteLine();
+
+            stopwatch.Restart();
+            for (int i = 0; i < testSize; ++i)
+            {
+                ProcessConcat();
+            }
+            stopwatch.Stop();
+            ts = stopwatch.Elapsed;
+            Console.WriteLine("ProcessConcat(), size - " + testSize.ToString());
+            Console.WriteLine("total: " + ts.ToString());
+            Console.Write("ProcessConcat(), mean - ");
+            Console.WriteLine((ts.TotalMilliseconds / testSize).ToString());
+            Console.WriteLine();
+        }
+    }
+}
