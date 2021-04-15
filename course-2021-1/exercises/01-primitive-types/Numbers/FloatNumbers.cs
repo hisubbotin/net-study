08e9982a18f0e73e77ab4b65e0b222df1c2089a7
Артём Нургалиев <artum44444@gmail.com>
2021-04-15 14:10:33 +0300

    solved tasks

--- course-2021-1/exercises/01-primitive-types/Numbers/FloatNumbers.cs
+++ course-2021-1/exercises/01-primitive-types/Numbers/FloatNumbers.cs
@@ -17,7 +17,8 @@ namespace Numbers
                 Необходимо вернуть значение, не используя непосредственно саму константу.
                 Для этого подумай, какой смысл в себе несет эта константа и где бы она могла стать результатом операции или вычисления функции.
             */
-            throw new NotImplementedException();
+            const double zero = 0;
+            return zero / zero;
         }
 
         /// <summary>
@@ -27,21 +28,22 @@ namespace Numbers
         /// <returns>True, если число имеет значение "не число", иначе false.</returns>
         internal static bool IsNaN(double d)
         {
-            // Подсказка: по аналогии с константами типа int, у типа double тоже есть свой набор констант.
-            throw new NotImplementedException();
+            return double.IsNaN(d);
         }
         
         /// <summary>
         /// Возвращает результат сравнения двух вещественнозначных чисел.
         /// </summary>
         /// <returns>-1 - первое меньше второго, 0 - значения равны, 1 - первое больше второго.</returns>
-        internal static int Compare(/* дополни сигнатуру метода как считаешь правильным */)
+        internal static int Compare(double first, double second, double tolerance)
         {
             /*
                 Подумай, почему это задание дано в части про вещественнозначные числа. И почему не дана полная сигнатура метода.
                 Если сходу идей нет, перестань искать подвох и просто реализуй дословно. Теперь еще раз посмотри на код и подумай в чем может быть проблема, сколько должно быть аргументов.
             */
-            throw new NotImplementedException();
+            if (Math.Abs(first - second) < tolerance) return 0;
+            if (first < second) return -1;
+            return 1;
         }
 
         // и все?!! О_о
--- course-2021-1/exercises/01-primitive-types/Numbers/Program.cs
+++ course-2021-1/exercises/01-primitive-types/Numbers/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
-
+using Numbers;
 [assembly: InternalsVisibleTo("Numbers.Tests")]
 
 /*
@@ -10,6 +10,8 @@ using System.Runtime.CompilerServices;
  * Как будешь готов[а], переходи в Integers.cs.
 */
 
+
+
 namespace Numbers
 {
     internal class Program
@@ -17,8 +19,18 @@ namespace Numbers
         private static void Main()
         {
             // твои бро: Console.WriteLine и Console.ReadLine
-
-            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine(Integers.HalfIntMaxValue());
+            Console.WriteLine(Integers.Cube(7));
+            // Console.WriteLine(Integers.CubeWithOverflowCheck(10000));
+            Console.WriteLine(Integers.CubeWithoutOverflowCheck(10000));
+            Console.WriteLine(Integers.TenTimes(11));
+            Console.WriteLine(Integers.ToHexString(15));
+            Console.WriteLine(FloatNumbers.GetNaN());
+            Console.WriteLine(FloatNumbers.IsNaN(FloatNumbers.GetNaN()));
+            Console.WriteLine(FloatNumbers.Compare(1, 2, 0.0000001));
+            Console.WriteLine(FloatNumbers.Compare(1.001, 0.999, 0.1));
+            Console.WriteLine(FloatNumbers.Compare(2, 1, 0));
         }
     }
 }
