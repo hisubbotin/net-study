19732f657fbe82dbc76c0c780358c6cdaa71797b
Артём Нургалиев <artum44444@gmail.com>
2021-04-29 22:31:36 +0300

    solved almost everything

--- course-2021-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
+++ course-2021-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub.Tests/RicksMercilessEncryptorTests.cs
@@ -5,6 +5,19 @@ namespace WubbaLubbaDubDub.Tests
 {
     public class RicksMercilessEncryptorTests
     {
+        [Theory]
+        [InlineData("Such, a? simple* example-example!; isn't it?", new string[]{"Such", "a", "simple", "example-example", "isn't", "it"} )]
+        public void TestSplitToWords(string line, string[] actual)
+        {
+            Assert.Equal(line.SplitToWords(), actual);
+        }
+
+        [Theory]
+        [InlineData("abc", "bcd")]
+        public void TestShiftInc(string word, string actual)
+        {
+            Assert.Equal(word.ShiftInc(), actual);
+        }
         
     }
 }
