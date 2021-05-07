fbcd1ae592a592a3e0536bec926698463ecbb374
rvolosenkov <roman.volosenkov@mail.ru>
2021-05-07 15:49:35 +0300

    task01

--- course-2021-1/exercises/01-primitive-types/Numbers/Program.cs
+++ course-2021-1/exercises/01-primitive-types/Numbers/Program.cs
@@ -19,6 +19,23 @@ namespace Numbers
             // твои бро: Console.WriteLine и Console.ReadLine
 
             Console.WriteLine("Hello World!");
+            Console.WriteLine(Numbers.Integers.HalfIntMaxValue());
+            Console.WriteLine(Numbers.Integers.Cube(3));
+            // Console.WriteLine(Numbers.Integers.CubeWithOverflowCheck(1234567891));
+            // Console.WriteLine(Numbers.Integers.CubeWithoutOverflowCheck(1234567891));
+            Console.WriteLine(Numbers.Integers.ToString(1234));
+            Console.WriteLine(Numbers.Integers.Parse("38216283"));
+            Console.WriteLine(Numbers.Integers.TenTimes(54321));
+            Console.WriteLine(Numbers.Integers.ToHexString(1234));
+            Console.WriteLine(Numbers.FloatNumbers.GetNaN());
+            Console.WriteLine(Numbers.FloatNumbers.IsNaN(Numbers.FloatNumbers.GetNaN()));
+            Console.WriteLine(Numbers.FloatNumbers.IsNaN(double.NaN));
+            Console.WriteLine(Numbers.FloatNumbers.IsNaN(7.55));
+            double x = 33.5321;
+            double y = 33.5320;
+            Console.WriteLine(Numbers.FloatNumbers.Compare(x, y, 0.001));
+            Console.WriteLine(Numbers.FloatNumbers.Compare(x, y, 0.0001));
+            Console.WriteLine(Numbers.FloatNumbers.Compare(y, x, 0.0001));
         }
     }
 }
