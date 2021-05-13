98c8823a8609c9a60c9089460924012d1476ef56
Aleksei Strashnov <alejo2000@yandex.ru>
2021-05-13 13:50:41 +0300

    updated

--- course-2021-1/exercises/01-primitive-types/Numbers/Integers.cs
+++ course-2021-1/exercises/01-primitive-types/Numbers/Integers.cs
@@ -46,7 +46,7 @@ namespace Numbers
                 Особенно это касается связки Visual Studio + Resharper, используя которую, если просто набрать return и нажать пробел,
                     то в появившемся списке автодополнения одной из первых будет нужная тебе константа :)
             */
-            return int.MaxValue / 2;
+            return int.MaxValue >> 1;
         }
 
         /// <summary>
