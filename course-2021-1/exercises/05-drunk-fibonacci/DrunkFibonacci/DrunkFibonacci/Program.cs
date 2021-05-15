13d5bc25e9de631fb9506728324f7b23c21b4844
Egorov Pavel <egorov.pavel@phystech.edu>
2021-05-15 19:55:26 +0300

    Done Task

--- course-2021-1/exercises/05-drunk-fibonacci/DrunkFibonacci/DrunkFibonacci/Program.cs
+++ course-2021-1/exercises/05-drunk-fibonacci/DrunkFibonacci/DrunkFibonacci/Program.cs
@@ -8,7 +8,12 @@ namespace DrunkFibonacci
         private static void Main()
         {
             Console.WriteLine("Hello World!");
+            var a = DrunkFibonacci.CreateIntArray(20); 
+            DrunkFibonacci.FillIntArray(a, 0, 1);
+            Console.WriteLine(a[19]);
 
+            Console.WriteLine("GetDrunkFibonacci: " + string.Join(',', DrunkFibonacci.GetDeterministicRandomSequence().Take(5)));
+            
             Console.WriteLine("GetDrunkFibonacci: " + string.Join(',', DrunkFibonacci.GetDrunkFibonacci().Take(100)));
             Console.WriteLine("GetMaxOnRange: " + DrunkFibonacci.GetMaxOnRange(1000, 400));
             Console.WriteLine("GetNextNegativeRange: " + string.Join(',', DrunkFibonacci.GetNextNegativeRange().Take(100)));
@@ -16,6 +21,7 @@ namespace DrunkFibonacci
             Console.WriteLine("GetInChunks: " + string.Join(Environment.NewLine, DrunkFibonacci.GetInChunks().Select(x => $"[{string.Join(',', x)}]").Take(3)));
             Console.WriteLine("FlattenChunkedSequence: " + string.Join(',', DrunkFibonacci.FlattenChunkedSequence().Take(60)));
             Console.WriteLine("GetGroupSizes: " + string.Join(',', DrunkFibonacci.GetGroupSizes()));
+            // Console.WriteLine(-17 % 8);
         }
     }
 }
