b90e9cd1cf4f7d3c2aa511c0f65db3a28507985a
Артём Нургалиев <artum44444@gmail.com>
2021-05-19 10:31:35 +0300

    added config to benchmark

--- course-2021-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/JoinTests/Program.cs
+++ course-2021-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/JoinTests/Program.cs
@@ -4,26 +4,45 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
+using Perfolizer.Horology;
 
 namespace JoinTests
 {
+    public class FastConfig : ManualConfig
+    {
+        public FastConfig()
+        {
+            Add(DefaultConfig.Instance);
+            AddJob(Job.Default.WithLaunchCount(1).WithIterationCount(5).WithWarmupCount(1));
+        }
+    }
+    
+    [Config(typeof(FastConfig))]
+    // [SimpleJob(RunStrategy.Monitoring, targetCount: 10, invocationCount: 10)]
     public class StringBuilderVsJoinVsConcat
     {
-        private readonly List<string> _data = new List<string>();
-        private readonly StringBuilder _stringBuilder = new StringBuilder();
-        // private static readonly Random random = new Random();
-        //
-        // private static string RandomString(int length) 
-        // { 
-        //     const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        //     return new string(Enumerable.Repeat(chars, length)
-        //         .Select(s => s[random.Next(s.Length)]).ToArray());
-        // }
-             
+        private readonly List<string> _data = new();
+        private readonly StringBuilder _stringBuilder = new();
+        private static readonly Random random = new Random();
+        
+        private static string RandomString(int length) 
+        { 
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
         public StringBuilderVsJoinVsConcat()
         {
-            _data = new List<string>() {"aa", "aa", "aa", "aa"};
+            var N = 1000000;
+            for (var i = 0; i < N; ++i)
+            {
+                _data.Add("aaaa");
+            }
         }
         
         [Benchmark] 
@@ -32,13 +51,13 @@ namespace JoinTests
             foreach (var s in _data) 
             { 
                 _stringBuilder.Append(s);
-            } 
+            }
             return _stringBuilder.ToString();
         }
         
         [Benchmark] 
         public string Join()
-        { 
+        {
             return string.Join("", _data);
         }
         
