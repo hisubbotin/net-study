d13c0ba3ea29e97052f993d386aaf06fa98ffaee
Azat <saginbaev30@gmail.com>
2021-05-20 10:44:06 +0300

    task 1 complete

--- course-2021-1/exercises/01-primitive-types/Numbers/FloatNumbers.cs
+++ course-2021-1/exercises/01-primitive-types/Numbers/FloatNumbers.cs
@@ -13,11 +13,8 @@ namespace Numbers
         /// </summary>
         internal static double GetNaN()
         {
-            /*
-                Необходимо вернуть значение, не используя непосредственно саму константу.
-                Для этого подумай, какой смысл в себе несет эта константа и где бы она могла стать результатом операции или вычисления функции.
-            */
-            throw new NotImplementedException();
+            double zero = 0;                
+            return zero / 0;
         }
 
         /// <summary>
@@ -27,21 +24,17 @@ namespace Numbers
         /// <returns>True, если число имеет значение "не число", иначе false.</returns>
         internal static bool IsNaN(double d)
         {
-            // Подсказка: по аналогии с константами типа int, у типа double тоже есть свой набор констант.
-            throw new NotImplementedException();
+            return double.IsNaN(d);
         }
         
         /// <summary>
         /// Возвращает результат сравнения двух вещественнозначных чисел.
         /// </summary>
         /// <returns>-1 - первое меньше второго, 0 - значения равны, 1 - первое больше второго.</returns>
-        internal static int Compare(/* дополни сигнатуру метода как считаешь правильным */)
+        internal static int Compare(double x, double y)
         {
-            /*
-                Подумай, почему это задание дано в части про вещественнозначные числа. И почему не дана полная сигнатура метода.
-                Если сходу идей нет, перестань искать подвох и просто реализуй дословно. Теперь еще раз посмотри на код и подумай в чем может быть проблема, сколько должно быть аргументов.
-            */
-            throw new NotImplementedException();
+
+            return Math.Sign(y - x);
         }
 
         // и все?!! О_о
