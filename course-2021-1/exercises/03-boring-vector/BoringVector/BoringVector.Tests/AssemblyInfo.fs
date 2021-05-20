b508044d2c64bc573402401623ed2f3b1ec66fa4
rvolosenkov <roman.volosenkov@mail.ru>
2021-05-20 09:19:35 +0300

    task03

---
+++ course-2021-1/exercises/03-boring-vector/BoringVector/BoringVector.Tests/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace BoringVector.Tests.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// Общие сведения о сборке можно задать с помощью следующего 
+// набора атрибутов. Отредактируйте эти значения атрибутов, чтобы изменить сведения,
+// связанные с этой сборкой.
+[<assembly: AssemblyTitle("BoringVector.Tests")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("BoringVector.Tests")>]
+[<assembly: AssemblyCopyright("Copyright ©  2021")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// При установке значения False в атрибуте ComVisible типы в этой сборке становятся невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте для атрибута ComVisible значение True для этого типа.
+[<assembly: ComVisible(false)>]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект видим для COM
+[<assembly: Guid("b55e87d0-2902-4167-8936-12ca376adf55")>]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+// 
+//       Основной номер версии
+//       Дополнительный номер версии 
+//       Номер сборки
+//      Редакция
+// 
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [<сборка: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ course-2021-1/exercises/03-boring-vector/BoringVector/ConsoleApp1/VectorTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Xunit;
+
+namespace BoringVector.Tests
+{
+    public class VectorTests
+    {
+        [Theory]
+        [InlineData(0, 0, 0)]
+        [InlineData(1, 2, 5)]
+        [InlineData(5, 50, 2525)]
+        public void Test_SquareLength(double x, double y, double ans)
+        {
+            Assert.Equal(new Vector(x, y).SquareLength(), ans);
+        }
+
+        [Theory]
+        [InlineData(0, 0, 0, 0, 0, 0)]
+        [InlineData(1, 0, -1, 0, 0, 0)]
+        [InlineData(3, 5, 0, 0, 3, 5)]
+        [InlineData(6, 10, -11, 2, -5, 12)]
+        public void Test_Add(double vX, double vY, double wX, double wY, double ansX, double ansY)
+        {
+            Vector ans = new Vector(vX, vY).Add(new Vector(wX, wY));
+            Assert.Equal(ans.X, ansX);
+            Assert.Equal(ans.Y, ansY);
+        }
+
+        [Theory]
+        [InlineData(0, 0, 2, 0, 0)]
+        [InlineData(0, 3, 6, 0, 18)]
+        [InlineData(1, 1, 3, 3, 3)]
+        [InlineData(2, -3, -1, -2, 3)]
+        public void Test_Scale(double vX, double vY, double k, double ansX, double ansY)
+        {
+            Vector ans = new Vector(vX, vY).Scale(k);
+            Assert.Equal(ans.X, ansX);
+            Assert.Equal(ans.Y, ansY);
+        }
+
+        [Theory]
+        [InlineData(0, 0, 1, 1, 0)]
+        [InlineData(0, 4, 7, 0, 0)]
+        [InlineData(1, -2, -1, 2, -5)]
+        [InlineData(10, 4, -2, 3, -8)]
+        public void Test_DotProduct(double vX, double vY, double wX, double wY, double ans)
+        {
+            Assert.Equal(new Vector(vX, vY).DotProduct(new Vector(wX, wY)), ans);
+        }
+
+        [Theory]
+        [InlineData(0, 0, 1, 1, 0)]
+        [InlineData(0, 4, 7, 0, 28)]
+        [InlineData(1, -2, -1, 2, 0)]
+        [InlineData(10, 4, -2, 3, 38)]
+        internal void TestCrossProduct(double vX, double vY, double wX, double wY, double ans)
+        {
+            Assert.Equal(new Vector(vX, vY).CrossProduct(new Vector(wX, wY)), ans);
+        }
+    }
+}
