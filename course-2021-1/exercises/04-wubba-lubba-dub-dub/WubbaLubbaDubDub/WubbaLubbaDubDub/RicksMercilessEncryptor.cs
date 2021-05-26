34f1998e86b8aa83a52b38a8df3a5feaeb56b9b8
TopCoder2K <pchelintsev.sv@phystech.edu>
2021-05-26 18:16:08 +0300

    Task was solved. Tests were passed. Benchmark was created.

--- course-2021-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub/RicksMercilessEncryptor.cs
+++ course-2021-1/exercises/04-wubba-lubba-dub-dub/WubbaLubbaDubDub/WubbaLubbaDubDub/RicksMercilessEncryptor.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace WubbaLubbaDubDub
 {
@@ -12,7 +14,7 @@ namespace WubbaLubbaDubDub
         public static string[] SplitToLines(this string text)
         {
             // У строки есть специальный метод. Давай здесь без регулярок
-            throw new NotImplementedException();
+            return text.Split('\n');
         }
 
         /// <summary>
@@ -21,7 +23,7 @@ namespace WubbaLubbaDubDub
         public static string[] SplitToWords(this string line)
         {
             // А вот здесь поиграйся с регулярками.
-            throw new NotImplementedException();
+            return Regex.Split(line, @"\W+");
         }
 
         /// <summary>
@@ -31,7 +33,7 @@ namespace WubbaLubbaDubDub
         public static string GetLeftHalf(this string s)
         {
             // у строки есть метод получения подстроки
-            throw new NotImplementedException();
+            return s.Substring(0, s.Length / 2);
         }
 
         /// <summary>
@@ -40,7 +42,7 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string GetRightHalf(this string s)
         {
-            throw new NotImplementedException();
+            return s.Substring(s.Length / 2);
         }
 
         /// <summary>
@@ -49,7 +51,7 @@ namespace WubbaLubbaDubDub
         public static string Replace(this string s, string old, string @new)
         {
             // и такой метод у строки, очевидно, тоже есть
-            throw new NotImplementedException();
+            return s.Replace(old, @new);
         }
 
         /// <summary>
@@ -65,7 +67,18 @@ namespace WubbaLubbaDubDub
                 FYI: локальную функцию можно объявлять даже после строки с return.
                 То же самое можно сделать и для всех оставшихся методов.
             */
-            throw new NotImplementedException();
+            string CharToCode(char ch) {
+                return String.Format("\\u{0:x4}", (int)ch);
+            }
+
+            var chars = s.ToCharArray();
+            var codes = new string[s.Length];
+            for (int i = 0; i < s.Length; ++i)
+            {
+                codes[i] = CharToCode(chars[i]);
+            }
+
+            return String.Concat(codes);
         }
 
         /// <summary>
@@ -77,7 +90,7 @@ namespace WubbaLubbaDubDub
                 Собрать строку из последовательности строк можно несколькими способами.
                 Один из низ - статический метод Concat. Но ты можешь выбрать любой.
             */
-            throw new NotImplementedException();
+            return String.Concat(s.Reverse());
         }
 
         /// <summary>
@@ -90,7 +103,21 @@ namespace WubbaLubbaDubDub
                 На минуту задержись здесь и посмотри, какие еще есть статические методы у char.
                 Например, он содержит методы-предикаты для определения категории Юникода символа, что очень удобно.
             */
-            throw new NotImplementedException();
+            var strWithReversedCase = new char[s.Length];
+            var chars = s.ToCharArray();
+
+            for (int i = 0; i < s.Length; ++i) 
+            {
+                if (char.IsUpper(s[i])) {
+                    strWithReversedCase[i] = char.ToLower(chars[i]);
+                }
+                else
+                {
+                    strWithReversedCase[i] = char.ToUpper(chars[i]);
+                }
+            }
+
+            return String.Concat(strWithReversedCase);
         }
 
         /// <summary>
@@ -99,7 +126,15 @@ namespace WubbaLubbaDubDub
         /// </summary>
         public static string ShiftInc(this string s)
         {
-            throw new NotImplementedException();
+            var shiftedStr = new char[s.Length];
+            var chars = s.ToCharArray();
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                shiftedStr[i] = (char)(chars[i] + 1);
+            }
+
+            return String.Concat(shiftedStr);
         }
 
 
@@ -117,7 +152,22 @@ namespace WubbaLubbaDubDub
                 Задача на поиграться с регулярками - вся сложность в том, чтобы аккуратно игнорировать комментарии.
                 Экспериментировать онлайн можно, например, здесь: http://regexstorm.net/tester и https://regexr.com/
             */
-            throw new NotImplementedException();
+            // Обычный Replace не умеет работать с регулярками.(((
+            var withoutComments = Regex.Replace(text, @"\/\/.*", string.Empty);  // убрали строчные комменты
+            withoutComments = Regex.Replace(
+                withoutComments, @"\/\*[^(\*\/)]*\*\/", string.Empty
+                );  // убрали блочные комменты
+
+            var matches = Regex.Matches(withoutComments, @"¶\w{4}:\w{4}¶");
+            var identifiers = new List<Int64>();
+            
+            foreach (var match in matches)
+            {
+                var convertedMatch = Regex.Replace(match.ToString(), @"[¶:]", string.Empty);
+                identifiers.Add(Convert.ToInt64(convertedMatch));
+            }
+
+            return identifiers.ToImmutableList();
         }
 
         #endregion
