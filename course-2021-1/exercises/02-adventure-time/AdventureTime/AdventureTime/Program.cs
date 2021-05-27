032cc0e57ec1388d8d90b6ad59b107e0bd2691bc
SweetWeakness <shirshin.dg@phystech.edu>
2021-05-27 12:14:19 +0300

    finally adv time

--- course-2021-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
+++ course-2021-1/exercises/02-adventure-time/AdventureTime/AdventureTime/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace AdventureTime
+﻿using System;
+
+namespace AdventureTime
 {
     internal class Program
     {
         private static void Main()
         {
+            Console.WriteLine(Time.WhatTimeIsIt());
+            Console.WriteLine(Time.WhatTimeIsItInUtc());
+            Console.WriteLine();
+
+            var dt_utc = Time.SpecifyKind(Time.WhatTimeIsIt(), DateTimeKind.Utc);
+            var dt_local = Time.SpecifyKind(Time.WhatTimeIsIt(), DateTimeKind.Local);
+            var dt_unspec = Time.SpecifyKind(Time.WhatTimeIsIt(), DateTimeKind.Unspecified);
+            Console.WriteLine(dt_utc);
+            Console.WriteLine(dt_local);
+            Console.WriteLine(dt_unspec);
+            Console.WriteLine();
+
+            Console.WriteLine(Time.ToRoundTripFormatString(dt_utc));
+            Console.WriteLine();
+
+            Console.WriteLine(Time.ParseFromRoundTripFormat(Time.ToRoundTripFormatString(dt_utc)));
+
+            Console.WriteLine();
+
+            Console.WriteLine(Time.AddTenSeconds(dt_utc));
+            Console.WriteLine(Time.AddTenSecondsV2(dt_utc));
+
+            Console.WriteLine();
+
+            Console.WriteLine(Time.GetHoursBetween(dt_utc, dt_utc.AddHours(3)));
+            Console.WriteLine(Time.GetHoursBetween(Time.WhatTimeIsItInUtc(), Time.WhatTimeIsIt()));
+            Console.WriteLine();
+            Console.WriteLine(Time.GetTotalMinutesInThreeMonths());
+            Console.WriteLine();
+
+
+            Console.WriteLine(Time.AreEqualBirthdays(dt_utc, dt_local));
         }
     }
 }
